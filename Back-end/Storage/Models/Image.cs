e20089217d1ed25e101fbb9e62bec61497ac4fb5
shalomich <shalo6947@gmail.com>
2021-02-16 22:37:09 +0700

    create class Image

---
+++ Back-end/Storage/Models/Image.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Storage.Models
+{
+    public class Image
+    {
+        private string _format;
+
+        public static readonly string[] FormatConsts = { "image/png", "image/jpeg" };
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public string Format
+        {
+            set
+            {
+                if (FormatConsts.Contains(value) == false)
+                    throw new ArgumentOutOfRangeException($"{value} не формат цифрового изображения");
+                _format = value;
+            }
+            get
+            {
+                return _format;
+            }
+        }
+        public string Endoding { set; get; }
+        public string Data { set; get; }
+    }
+}
