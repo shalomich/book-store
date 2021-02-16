7047c84531aee76eba14f15833947cbb0a2f7e2c
shalomich <shalo6947@gmail.com>
2021-02-16 18:16:37 +0700

    create class Entity

---
+++ Back-end/Storage/Models/Entity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Storage.Models
+{
+    public abstract class Entity
+    {
+        public int Id { set; get; }
+    }
+}
