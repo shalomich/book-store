81e66b5eeb0ecb0907f036bf5e4cd16a34f83808
shalomich <shalo6947@gmail.com>
2021-02-16 22:39:58 +0700

    add database configuration for entity

---
+++ Back-end/Storage/DatabaseConfigs/EntityDbConfig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Storage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Storage.DatabaseConfigs
+{
+    abstract public class EntityDbConfig<T> : IEntityTypeConfiguration<T> where T : Entity
+    {
+        public virtual void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder.Property(entity => entity.TitleImageName).IsRequired();
+            builder.Property(entity => entity.Images).IsRequired();
+        }
+    }
+}
