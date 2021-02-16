975e31762bf191cbacd7bd16c9b5f73745ea74be
shalomich <shalo6947@gmail.com>
2021-02-16 22:48:00 +0700

    add database configuration for author

---
+++ Back-end/Storage/DatabaseConfigs/AuthorDbConfig.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Storage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Storage.DatabaseConfigs
+{
+    public class AuthorDbConfig : EntityDbConfig<Author>
+    {
+        public override void Configure(EntityTypeBuilder<Author> builder)
+        {
+            builder.ToTable("authors");
+
+            builder.Property(author => author.Name).IsRequired();
+            builder.Property(author => author.Surname).IsRequired();
+            
+            builder.HasIndex(author => new { author.Name, author.Surname, author.BirthDate }).IsUnique();
+        }
+    }
+}
