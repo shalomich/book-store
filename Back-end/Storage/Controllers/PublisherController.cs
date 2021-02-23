cbcc3d9526cb65d2864af76377d0d1d33071a4e8
shalomich <shalo6947@gmail.com>
2021-02-23 23:03:20 +0700

    create class PublisherController

---
+++ Back-end/Storage/Controllers/PublisherController.cs
@@ -0,0 +1,15 @@
+﻿using Storage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Storage.Controllers
+{
+    public class PublisherController : EntityController<Publisher>
+    {
+        public PublisherController(Database database) : base(database)
+        {
+        }
+    }
+}
