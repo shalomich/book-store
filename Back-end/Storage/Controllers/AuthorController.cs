97ed8ee9788cabbe02da11b629af721792169670
shalomich <shalo6947@gmail.com>
2021-02-23 23:02:58 +0700

    create class AuthorController

---
+++ Back-end/Storage/Controllers/AuthorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Storage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Storage.Controllers
+{
+    public class AuthorController : EntityController<Author>
+    {
+        public AuthorController(Database database) : base(database)
+        {
+        }
+    }
+}
