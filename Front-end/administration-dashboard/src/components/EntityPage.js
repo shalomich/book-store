44c7283b7db1379d14a49d091eaea77489e7f9c6
Gevorg17 <ggevorgyan017@gmail.com>
2021-02-28 21:37:48 +0700

    added styles for entity page components

---
+++ Front-end/administration-dashboard/src/components/EntityPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ItemsList from "./ItemsList/ItemsList";
+import {render} from "@testing-library/react";
+import {Redirect, withRouter} from "react-router-dom";
+
+const EntityPage = (props) => {
+    let clicked = false;
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        props.history.push("/admin")
+    }
+
+    return (
+        <div>
+            <button onClick={handleClick}>На главную</button>
+            <h1 style={{marginLeft: "65px"}}>{props.name}</h1>
+            <ItemsList />
+        </div>
+    );
+}
+
+export default withRouter(EntityPage);
