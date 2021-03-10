af2cb0d260e4b46cfb7cf2516fd2d225256fdc68
Gevorg17 <ggevorgyan017@gmail.com>
2021-03-10 18:38:53 +0700

    changed entity page forms data

--- Front-end/administration-dashboard/src/components/Item/Item.js
+++ Front-end/administration-dashboard/src/components/Item/Item.js
@@ -8,6 +8,13 @@ class Item extends Component {
         this.link = "/form";
         this.clicked = false;
         const information = Object.entries(this.props.item);
+        information.splice(2);
+        this.imageSource = '';
+        this.props.item.images.forEach((image) => {
+            if (image.name === this.props.item.titleImageName) {
+                this.imageSource = `data:${image.format};${image.encoding},${image.data}`;
+            }
+        })
         this.info = information.map((field) =>
             <div className={style.item}>
                 <span>{field[0]}: </span>
@@ -15,7 +22,6 @@ class Item extends Component {
             </div>
         );
     }
-
 
 
     handleClick = () => {
@@ -31,10 +37,13 @@ class Item extends Component {
 
         return (
             <div className={style.item_block}>
+                <img className={style.image} src={this.imageSource}/>
                 {this.info}
-                <hr className={style.line}/>
-                <button className={style.edit_button} onClick={this.handleClick}>Изменить</button>
-                <button className={style.delete_button} onClick={this.handleClick}>Удалить</button>
+                <div className={style.button_block}>
+                    <hr className={style.line}/>
+                    <button className={style.edit_button} onClick={this.handleClick}>Изменить</button>
+                    <button className={style.delete_button} onClick={this.handleClick}>Удалить</button>
+                </div>
             </div>
         );
     }
