1114413120976d6e86137fcf92a132bbdccf5787
shalomich <shalo6947@gmail.com>
2021-03-22 19:30:11 +0700

    upgrade class EntityToFormConverter

--- Back-end/Storage/Services/EntityToFormConverter.cs
+++ Back-end/Storage/Services/EntityToFormConverter.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Storage.Services
@@ -12,75 +13,84 @@ namespace Storage.Services
     {
         private class PropertyToFormElement
         {
-            public readonly Type PropertyType;
-            
-            public readonly string[] FormElements;
-            
-            public readonly string DefaultFormElement;
-            public PropertyToFormElement(Type propertyType, string[] formElements, string defaultElement)
-            {
-                PropertyType = propertyType;
-                FormElements = formElements;
-
-                if (FormElements.Contains(defaultElement) == false)
-                    throw new ArgumentException();
-                DefaultFormElement = defaultElement;
+            private string _typeName;
+            private string _defaultFormElement;
+            public string TypeName {
+                set 
+                {
+                    try
+                    {
+                        Type.GetType(value);
+                        _typeName = value;
+                    }
+                    catch (Exception)
+                    {
+                        throw new ArgumentException(); 
+                    }
+                } 
+                get 
+                {
+                    return _typeName;
+                } 
+            }        
+            public string[] FormElements { set; get; }
+            public string DefaultFormElement { 
+                set 
+                {
+                    if (FormElements.Contains(value) == false)
+                        throw new ArgumentOutOfRangeException();
+                    _defaultFormElement = value;
+                } 
+                get 
+                {
+                    return _defaultFormElement;
+                } 
             }
         }
         
-
-        private const string _entitySection = "form:entity";
-        private const string _settingsSection = "form:settings";
+        private const string _entitiesSectionName = "form:entity";
+        private const string _settingsSectionName = "form:settings";
         
-        private readonly ImmutableArray<PropertyToFormElement> _defaultSettings;
-        private readonly Dictionary<string, Dictionary<string, string>> _entityConfigSettings;
+        private readonly IEnumerable<PropertyToFormElement> _defaultSettings;
+        private IConfigurationSection _entitiesSection;
         public EntityToFormConverter(IConfiguration configuration) 
         {
+            
             _defaultSettings = configuration
-                .GetSection(_settingsSection)
-                .GetChildren()
-                .Select(section => new PropertyToFormElement(
-                    Type.GetType(section["propertyType"]),
-                    section.GetSection("formElements")
-                            .GetChildren()
-                            .Select(section => section.Value)
-                            .ToArray(),
-                    section["defaultFormElement"]))
-                .ToImmutableArray();
-            _entityConfigSettings = configuration
-                .GetSection(_entitySection)
-                .GetChildren()
-                .ToDictionary(
-                    entitySection => entitySection.Key,
-                    entitySection => entitySection
-                    .GetChildren()
-                    .ToDictionary(
-                        propertySection => propertySection.Key,
-                        propertySection => propertySection.Value));                                              
+                .GetSection(_settingsSectionName)
+                .Get<PropertyToFormElement[]>();
+
+            _entitiesSection = configuration.GetSection(_entitiesSectionName);                                         
         }
 
         public Dictionary<string,string> Convert<T>() where T : Entity
         {
-            var convertedType = typeof(T);
+            Type convertedType = typeof(T);
+            PropertyInfo[] properties = convertedType.GetProperties();
 
-            var propertyConfigSettings = _entityConfigSettings[convertedType.Name];
+            IConfigurationSection currentEntitySection = _entitiesSection.GetSection(convertedType.Name);
+            Dictionary<string, string> entitySettings = currentEntitySection
+                                                            .GetChildren()
+                                                            .ToDictionary(
+                                                                section => section.Key,
+                                                                section => section.Value);
             
-            var properties = convertedType.GetProperties();
-
             var finalSettings = new Dictionary<string, string>();
 
             foreach (var property in properties)
             {
-                var propertyToFormElement = _defaultSettings.FirstOrDefault(propertyToFormElement => propertyToFormElement.PropertyType == property.PropertyType);
-                if (propertyToFormElement == null)
+                PropertyToFormElement currentElement = _defaultSettings.FirstOrDefault(currentElement => 
+                                                Type.GetType(currentElement.TypeName) == property.PropertyType);
+                if (currentElement == null)
                     continue;
 
+                string currentFormElement = currentElement.DefaultFormElement;
 
-                if (propertyConfigSettings.TryGetValue(property.Name, out string formElement) == true)
-                    if (propertyToFormElement.FormElements.Contains(formElement) == true)
-                        finalSettings.Add(property.Name, formElement);
-                    else finalSettings.Add(property.Name, propertyToFormElement.DefaultFormElement);
-                else finalSettings.Add(property.Name, propertyToFormElement.DefaultFormElement);
+                if (entitySettings.TryGetValue(property.Name, out string formElement) == true)
+                    if (currentElement.FormElements.Contains(formElement) == true)
+                        currentFormElement = formElement;
+                
+                finalSettings.Add(property.Name, currentFormElement);
             }
 
             return finalSettings;
