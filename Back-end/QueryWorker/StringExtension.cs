318901296731f858b4b21cada0cee1388a5ca368
shalomich <shalo6947@gmail.com>
2021-03-23 22:29:14 +0700

    create class StringExtension

---
+++ Back-end/QueryWorker/StringExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueryWorker
+{
+    public static class StringExtension
+    {
+        public static string ToCapitalLetter(this string str)
+        {
+            return str.Substring(0, 1).ToUpper() + (str.Length > 1 ? str.Substring(1) : "");
+        }
+    }
+}
