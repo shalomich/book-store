98d8bee075e4191287da971f0018836647115f04
shalomich <shalo6947@gmail.com>
2021-03-23 22:30:11 +0700

    create interface IQueryNode

---
+++ Back-end/QueryWorker/QueryNodes/IQueryNode.cs
@@ -0,0 +1,13 @@
+﻿using QueryWorker.Visitors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QueryWorker
+{
+    public interface IQueryNode
+    {
+        IQueryable<T> Execute<T>(IQueryable<T> query);
+    }
+}
