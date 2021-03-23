041e26efacd06d7c9a8c45b9858b6f2c3d7aab5f
shalomich <shalo6947@gmail.com>
2021-03-23 22:33:20 +0700

    add factories for query nodes

---
+++ Back-end/QueryWorker/Factories/FilterFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueryWorker.Factories
+{
+    class FilterFactory : IQueryNodeFactory
+    {
+        public string[] FilteredProperties { set; get; }
+        public IQueryNode Create()
+        {
+            return new Filter() { FilteredProperties = FilteredProperties};
+        }
+    }
+}
---
+++ Back-end/QueryWorker/Factories/SortingFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueryWorker.Factories
+{
+    class SortingFactory : IQueryNodeFactory
+    {
+        public string[] SortedProperties { set; get; }
+        public IQueryNode Create()
+        {
+            return new Sorting() { SortedProperties = SortedProperties};
+        }
+    }
+}
