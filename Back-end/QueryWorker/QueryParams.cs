3980ebca7d620812f23dd1b19e1feafaa7a3078a
shalomich <shalo6947@gmail.com>
2021-03-23 22:35:40 +0700

    create class QueryParams

---
+++ Back-end/QueryWorker/QueryParams.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueryWorker
+{
+    public class QueryParams
+    {
+        public string[] Filter { set; get; } = new string[] { };
+        public string[] Sorting { set; get; } = new string[] { };
+        public string Pagging { set; get; } = String.Empty;
+       
+    }
+}
