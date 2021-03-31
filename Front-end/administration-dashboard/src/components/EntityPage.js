2e10f7e13e026e1a2c1812e583cd38863925e6fb
Gevorg17 <ggevorgyan017@gmail.com>
2021-03-31 22:10:27 +0700

    added get request

--- Front-end/administration-dashboard/src/components/EntityPage.js
+++ Front-end/administration-dashboard/src/components/EntityPage.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ItemsList from "./ItemsList/ItemsList";
 import {render} from "@testing-library/react";
 import {Redirect, withRouter} from "react-router-dom";
+import axios from "axios";
+import {getData} from "../API/api";
 
 const EntityPage = (props) => {
     let clicked = false;
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        axios.get("https://localhost:44327/storage/publication")
+            .then(res => {
+                if(res.status===200){
+                    setData(res.data)
+                }
+            });
+    }, [setData]);
 
     const handleClick = (e) => {
         e.preventDefault();
         props.history.push("/admin")
     }
 
+    if (!data) {
+        return (<div>
+            <p>Данные отстутствуют</p>
+        </div>)
+    }
+
     return (
         <div>
             <button onClick={handleClick}>На главную</button>
             <h1 style={{marginLeft: "65px", marginBottom: "50px"}}>{props.name}</h1>
-            <ItemsList />
+            <ItemsList data = {data} {...props} />
         </div>
     );
 }
--- Front-end/administration-dashboard/src/components/PagesManager.js
+++ Front-end/administration-dashboard/src/components/PagesManager.js
@@ -3,24 +3,29 @@ import MainPage from "./MainPage";
 import {BrowserRouter, Route, Redirect} from "react-router-dom";
 import {Component} from "react/cjs/react.production.min";
 import EntityPage from "./EntityPage";
+import ItemForm from "./ItemForm/ItemForm";
 
 class PagesManager extends Component{
     constructor(props) {
         super(props);
-        this.state = {product: String()}
+        this.state = {
+            product: String(),
+            type: String(),
+            action: String()
+        }
     }
 
-    updateData = (value) => {
-        this.setState({ product: value })
+    updateData = (product, type) => {
+        this.setState({ product: product, type: type })
     }
 
     render() {
         return (
             <BrowserRouter>
                 <div>
-                    <Redirect from="/" to="/admin" />
                     <Route exact path="/admin" render={() => <MainPage updateData = {this.updateData} goods = {this.props.goods}/>}/>
-                    <Route strict path="/admin/" render={() => <EntityPage name={this.state.product} />}/>
+                    <Route exact path="/admin/:String" render={() => <EntityPage updateData = {this.updateData} type={this.state.type} name={this.state.product} />}/>
+                    <Route path="/admin/:String/form" render={() => <ItemForm type={this.state.type} name={this.state.product}/>}/>
                 </div>
             </BrowserRouter>
         );
