9eae8c1783c328d7bbc176ece08a4d4f4fda6bed
shalomich <shalo6947@gmail.com>
2021-04-03 23:29:18 +0700

    create class Informer

---
+++ Back-end/QueryWorker/Informer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueryWorker
+{
+    public class Informer
+    {
+        private List<KeyValuePair<string, string>> _messages = new List<KeyValuePair<string, string>>();
+        private int _errorCount = 0;
+        public IReadOnlyCollection<KeyValuePair<string, string>> Messages => _messages;
+
+        public void Subscribe(IInformed informed)
+        {
+            informed.Accepted += AddMessage;
+            informed.Crashed += AddErrorMessage;
+        }
+
+        public void Unsubscribe(IInformed informed)
+        {
+            informed.Accepted -= AddMessage;
+            informed.Crashed -= AddErrorMessage;
+        }
+        private void AddMessage(string theme,string text)
+        {
+            _messages.Add(KeyValuePair.Create(theme, text));
+        }
+
+        private void AddErrorMessage(string theme,string text)
+        {
+            _errorCount ++;
+            AddMessage($"error{_errorCount}", $"{theme}: {text}");
+        }
+    }
+}
