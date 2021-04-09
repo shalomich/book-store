1c779c5c3038db3f4a64793b68ed264fcd8b6c28
Gevorg17 <ggevorgyan017@gmail.com>
2021-04-09 21:48:32 +0700

    added put request

--- Front-end/administration-dashboard/src/components/ProductsList/ProductsList.js
+++ Front-end/administration-dashboard/src/components/ProductsList/ProductsList.js
@@ -3,8 +3,8 @@ import Product from "../Product/Product";
 import style from "./ProductsList.module.css"
 
 const ProductsList = (props) => {
-    const items = props.goods.map((item) =>
-        <Product updateData = {props.updateData} goods = {item} />
+    const items = props.goods.map((item, index) =>
+        <Product key={index} updateData = {props.updateData} goods = {item} />
     );
 
     return (
