698a5f7d60a54a75ab279f2192592cf6c6397934
shalomich <shalo6947@gmail.com>
2021-04-21 16:32:07 +0700

    create class User

---
+++ Back-end/Auth/Models/User.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Various;
+
+namespace Auth.Models
+{
+    public class User : IdentityUser
+    {
+        private const int _minAge = 12;
+        private const int _maxAge = 80;
+
+        private static readonly string _minAgeMessage;
+        private static readonly string _maxAgeMessage;
+
+        private int _age;
+
+        static User()
+        {
+            _minAgeMessage = ExceptionMessages.GetMessage(ExceptionMessageType.Less, nameof(Age), _minAge.ToString());
+            _maxAgeMessage = ExceptionMessages.GetMessage(ExceptionMessageType.More, nameof(Age), _maxAge.ToString());
+        }
+
+        public int Age
+        {
+            set
+            {
+                if (value < _minAge)
+                    throw new ArgumentException(_minAgeMessage);
+                if (value > _maxAge)
+                    throw new ArgumentException(_maxAgeMessage);
+                _age = value;
+            }
+            get
+            {
+                return _age;
+            }
+        }
+    }
+}
