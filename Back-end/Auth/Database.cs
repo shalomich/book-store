15da8c237fe6e1330c58875a0ab2f1d00a1d771c
shalomich <shalo6947@gmail.com>
2021-04-21 16:47:25 +0700

    first migration for auth database

---
+++ Back-end/Auth/Database.cs
@@ -0,0 +1,18 @@
+﻿using Auth.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth
+{
+    public class Database : IdentityDbContext<User>
+    {
+        public Database(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
