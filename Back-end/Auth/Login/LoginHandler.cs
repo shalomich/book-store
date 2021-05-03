5f40d53a2e5bf60b1a0675299896d7273190258e
shalomich <shalo6947@gmail.com>
2021-05-03 17:53:01 +0700

    create classes for login

---
+++ Back-end/Auth/Login/LoginHandler.cs
@@ -0,0 +1,51 @@
+﻿using Auth.Exceptions;
+using Auth.Models;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Auth.Login
+{
+	public class LoginHandler : IRequestHandler<LoginQuery, AuthAnswer>
+	{
+		private static readonly RestError _emailUnregistered = new RestError { Reason = "EmailUnregistered", Message = "This email is unregistered" };
+		private static readonly RestError _passwordUncorrect = new RestError { Reason = "PasswordUncorrect", Message = "Wrong password" };
+
+		private readonly UserManager<User> _userManager;
+
+		private readonly SignInManager<User> _signInManager;
+
+		public LoginHandler(UserManager<User> userManager,
+									   SignInManager<User> signInManager)
+		{
+			_userManager = userManager;
+			_signInManager = signInManager;
+		}
+
+		public async Task<AuthAnswer> Handle(LoginQuery request, CancellationToken cancellationToken)
+		{
+			var user = await _userManager.FindByEmailAsync(request.Email);
+			if (user == null)
+				throw new RestException(HttpStatusCode.BadRequest, new List<RestError>() { _emailUnregistered});
+			
+
+			var result = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
+
+			if (result.Succeeded)
+			{
+				return new AuthAnswer
+				{
+					Id = user.Id,
+					Token = "test",
+				};
+			}
+			else throw new RestException(HttpStatusCode.BadRequest, new List<RestError>() { _passwordUncorrect});
+			
+		}
+	}
+}
