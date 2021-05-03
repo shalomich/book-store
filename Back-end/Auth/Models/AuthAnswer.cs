7e1b62d52b715db9eb8bdfb0305a9738d5d23b86
shalomich <shalo6947@gmail.com>
2021-05-03 17:52:04 +0700

    create class AuthAnswer

---
+++ Back-end/Auth/Models/AuthAnswer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth.Models
+{
+    public class AuthAnswer
+    {
+        public string Id { set; get; }
+        public string Token { set; get; }
+    }
+}
