15f86c883aff2930bc2fc909cd9510ccf1019437
shalomich <shalo6947@gmail.com>
2021-05-03 17:54:49 +0700

    create class UserController

---
+++ Back-end/Auth/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Auth.Login;
+using Auth.Models;
+using Auth.Registration;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth.Controllers
+{
+    [AllowAnonymous]
+    [Route("auth")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthAnswer>> LoginAsync(LoginQuery query)
+        {
+            return await _mediator.Send(query);
+        }
+
+        [HttpPost("registration")]
+        public async Task<ActionResult<AuthAnswer>> RegistrationAsync(RegistrationCommand command)
+        {
+            return await _mediator.Send(command);
+        }
+    }
+}
