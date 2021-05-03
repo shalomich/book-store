7938236beaae9c31c5b63e07f5c0b2e09ef3a776
shalomich <shalo6947@gmail.com>
2021-05-03 21:52:33 +0700

    create interface IJwtGenerator

---
+++ Back-end/Auth/Services/IJwtGenerator.cs
@@ -0,0 +1,13 @@
+﻿using Auth.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth.Services
+{
+    public interface IJwtGenerator
+    {
+        string CreateToken(User user, string role);
+    }
+}
