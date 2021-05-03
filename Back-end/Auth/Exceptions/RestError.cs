6b6ad26f9eae136cdec465432372d57c9d0b0842
shalomich <shalo6947@gmail.com>
2021-05-03 17:47:55 +0700

    create class RestException

---
+++ Back-end/Auth/Exceptions/RestError.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth.Exceptions
+{
+    public class RestError
+    {
+        public string Reason { set; get; }
+        public string Message { set; get; }
+    }
+}
---
+++ Back-end/Auth/Exceptions/RestException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Auth.Exceptions
+{
+    public class RestException : Exception
+    {
+        public HttpStatusCode Code { get; }
+        
+        public IEnumerable<RestError> Errors;
+
+        public RestException(HttpStatusCode code, IEnumerable<RestError> errors)
+        {
+            Code = code;
+            Errors = errors ?? throw new ArgumentNullException(nameof(errors));
+        }
+    }
+}
