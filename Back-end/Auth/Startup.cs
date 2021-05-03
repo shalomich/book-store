9b77a600cac30946cce9d24ddb80a76e5ecec55e
shalomich <shalo6947@gmail.com>
2021-05-03 17:55:29 +0700

    add settings for identity

--- Back-end/Auth/Startup.cs
+++ Back-end/Auth/Startup.cs
@@ -1,9 +1,19 @@
+using Auth.Login;
+using Auth.Middlewares;
+using Auth.Models;
+using MediatR;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
@@ -24,6 +34,26 @@ namespace Auth
         {
             services.AddDbContext<Database>(options => 
                 options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddMediatR(typeof(LoginHandler).Assembly);
+            services.TryAddSingleton<ISystemClock, SystemClock>();
+
+            services.AddMvc(option =>
+            {
+                option.EnableEndpointRouting = false;
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+                option.Filters.Add(new AuthorizeFilter(policy));
+            }).SetCompatibilityVersion(CompatibilityVersion.Latest);
+
+            var builder = services.AddIdentityCore<User>();
+            var identityBuilder = new IdentityBuilder(builder.UserType, builder.Services);
+
+            identityBuilder.AddEntityFrameworkStores<Database>();
+            identityBuilder.AddSignInManager<SignInManager<User>>();
+
+            
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -33,12 +63,11 @@ namespace Auth
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            app.UseMiddleware<ErrorHandlingMiddleware>();
 
-            app.UseEndpoints(endpoints =>
-            {
-                
-            });
+            app.UseAuthentication();
+
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
