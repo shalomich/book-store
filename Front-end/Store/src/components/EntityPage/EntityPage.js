627ea195815f8b9c13177693a495f2d0fbaf7458
Gevorg17 <45511485+Gevorg17@users.noreply.github.com>
2021-05-12 18:03:05 +0700

    added EntityPage component

--- Front-end/Store/src/components/EntityPage/EntityPage.js
+++ Front-end/Store/src/components/EntityPage/EntityPage.js
@@ -1,10 +1,21 @@
-function EntityPage() {
-    const uri = 'https://localhost:44327/storage/publication/201'
-    return (
-        <div>
+import {PublicationCard} from '../EntityCard/EntityCard'
+import ToApiEntity from "../HOC/ToApiEntity";
 
-        </div>
-    );
+const Cards = new Map([['publication',PublicationCard]])
+
+const EntityPage = ({match}) => {
+    const {entityName, id} = match.params;
+    const uri = `https://localhost:44327/storage/${entityName}/${id}`
+
+    let Card = Cards.get(entityName);
+    Card = ToApiEntity(Card,uri)
+
+    return (
+        <>
+            <a href="/store">Назад</a>
+            <Card/>
+        </>
+    )
 }
 
 export default EntityPage;
