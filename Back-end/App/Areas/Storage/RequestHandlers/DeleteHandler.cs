81c02a768faa82cec49d7f56eb48ec3e37e67315
shalomich <shalo6947@gmail.com>
2021-08-07 15:23:01 +0700

    create class DeleteHandler

---
+++ Back-end/App/Areas/Storage/RequestHandlers/DeleteHandler.cs
@@ -0,0 +1,35 @@
+﻿using App.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using static App.Areas.Storage.RequestHandlers.DeleteHandler;
+
+namespace App.Areas.Storage.RequestHandlers
+{
+    public class DeleteHandler : IRequestHandler<DeleteCommand, Unit>
+    {
+        public record DeleteCommand(Entity Entity) : IRequest<Unit>;
+        private ApplicationContext Context { get; }
+        public DeleteHandler(ApplicationContext context)
+        {
+            Context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        public async Task<Unit> Handle(DeleteCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                Context.Remove(request.Entity);
+                await Context.SaveChangesAsync();
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+
+            return Unit.Value;
+        }
+    }
+}
