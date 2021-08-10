c55ddc50957607694b1e6824822080fdc89f5abe
shalomich <shalo6947@gmail.com>
2021-08-11 01:13:32 +0700

    create class ProductForm

---
+++ Back-end/App/Areas/Storage/ViewModels/Forms/ProductForm.cs
@@ -0,0 +1,28 @@
+﻿using App.Entities;
+using App.Entities.Products;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Areas.Storage.ViewModels
+{
+    public record ProductForm : EntityForm
+    {
+        [Required]
+        public string Name { init; get; }
+
+        [Required]
+        [Range(Product.MinCost, int.MaxValue)]
+        public int Cost { init; get; }
+
+        [Required]
+        public uint Quantity { set; get; }
+
+        [MaxLength(Product.MaxDescriptionLength)]
+        public string Description { set; get; }
+
+        public AlbumForm Album { set; get; }
+    }
+}
