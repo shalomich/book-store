8fcc7d098e1019c0a3cde806fd00235b0e678f6d
shalomich <shalo6947@gmail.com>
2021-08-11 15:52:39 +0700

    create FormModel attribute

--- Back-end/App/Areas/Storage/ViewModels/Forms/CoverArtForm.cs
+++ Back-end/App/Areas/Storage/ViewModels/Forms/CoverArtForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using App.Areas.Storage.Attributes.FormModel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,9 +7,11 @@ using System.Threading.Tasks;
 
 namespace App.Areas.Storage.ViewModels
 {
+    [FormModel]
     public record CoverArtForm : EntityForm
     {
         [Required]
+        [FormField(FormFieldType.Text,"Тип обложки")]
         public string Name { init; get; }
     }
 }
--- Back-end/App/Areas/Storage/ViewModels/Forms/ProductForm.cs
+++ Back-end/App/Areas/Storage/ViewModels/Forms/ProductForm.cs
@@ -1,4 +1,5 @@
-﻿using App.Entities;
+﻿using App.Areas.Storage.Attributes.FormModel;
+using App.Entities;
 using App.Entities.Products;
 using System;
 using System.Collections.Generic;
@@ -11,18 +12,23 @@ namespace App.Areas.Storage.ViewModels
     public record ProductForm : EntityForm
     {
         [Required]
+        [FormField(FormFieldType.Text, "Название товара")]
         public string Name { init; get; }
 
         [Required]
         [Range(Product.MinCost, int.MaxValue)]
+        [FormField(FormFieldType.Number, "Цена")]
         public int Cost { init; get; }
 
         [Required]
+        [FormField(FormFieldType.Number, "Количество товара")]
         public uint Quantity { set; get; }
 
         [MaxLength(Product.MaxDescriptionLength)]
+        [FormField(FormFieldType.TextArea, "Описание",false)]
         public string Description { set; get; }
 
+        [FormField(FormFieldType.Image, "Изображения товара",false)]
         public AlbumForm Album { set; get; }
     }
 }
