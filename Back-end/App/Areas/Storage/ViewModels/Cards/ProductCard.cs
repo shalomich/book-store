5d6f04f74d5ac9187f4ca58d561951b11b3abfd8
shalomich <shalo6947@gmail.com>
2021-08-13 01:09:38 +0700

    create class ProductCard

---
+++ Back-end/App/Areas/Storage/ViewModels/Cards/ProductCard.cs
@@ -0,0 +1,17 @@
+﻿using App.Entities.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Areas.Storage.ViewModels.Cards
+{
+    public record ProductCard
+    {
+        public int Id { init; get; }
+        public string Name { init; get; }
+        public int Cost { init; get; }
+        public ImageDto TitleImage { init; get; }
+    }
+   
+}
