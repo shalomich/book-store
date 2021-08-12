2e0b7da604bc6606987f013fc2e0eb7b8e5b4c8d
shalomich <shalo6947@gmail.com>
2021-08-13 01:10:07 +0700

    create class PublicationCard

---
+++ Back-end/App/Areas/Storage/ViewModels/Cards/PublicationCard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Areas.Storage.ViewModels.Cards
+{
+    public record PublicationCard : FullProductCard
+    {
+        public string Isbn { init; get; }
+        public int ReleaseYear { init; get; }
+        public string AuthorName { init; get; }
+        public string PublisherName { init; get; }
+        public string Type { init; get; }
+        public string[] Genres { init; get; }
+        public string OriginalName { init; get; }
+        public string AgeLimit { init; get; }
+        public string CoverArt { init; get; } 
+        public string PublicationFormat { init; get; }    
+        public int? PageQuantity { init; get; }
+     
+    }
+}
