2d1f505935b23b67f71cb65c51b63cc143663fd1
shalomich <shalo6947@gmail.com>
2021-08-19 12:51:22 +0700

    create class ConfigurationFinder

---
+++ Back-end/QueryWorker/Configurations/ConfigurationFinder.cs
@@ -0,0 +1,35 @@
+﻿using QueryWorker.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryWorker.Configurations
+{
+    internal class ConfigurationFinder
+    {
+        private const string NotFoundConfigTemplateMesage = "Not found query configuration for type {0}";
+        private readonly Assembly _assembly;
+        public ConfigurationFinder(Assembly assembly)
+        {
+            _assembly = assembly ?? throw new ArgumentNullException(nameof(assembly));
+        }
+
+        public QueryConfiguration<T> Find<T>() where T : class 
+        {
+            var type = typeof(T);
+            var configurationType = typeof(QueryConfiguration<>).MakeGenericType(type);
+           
+            var configuration = _assembly.GetTypes()
+                .SingleOrDefault(type => type.IsSubclassOf(configurationType)
+                    && type.IsAbstract == false);
+
+            if (configuration == null)
+                throw new InvalidOperationException(string.Format(NotFoundConfigTemplateMesage, type.Name));
+
+            return Activator.CreateInstance(configuration) as QueryConfiguration<T>;
+        }
+    }
+}
