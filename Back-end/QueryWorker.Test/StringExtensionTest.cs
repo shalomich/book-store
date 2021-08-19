30241fe7477a264477d810c4971e0118ef07b459
shalomich <shalo6947@gmail.com>
2021-08-19 14:22:06 +0700

    create class StringExtension

---
+++ Back-end/QueryWorker.Test/StringExtensionTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using QueryWorker.Extensions;
+
+namespace QueryWorker.Test
+{
+    public class StringExtensionTest
+    {
+        [Theory]
+        [MemberData(nameof(GetSubstringsByLengthTestData))]
+        public void SubstringsByLengthTest(string str, int substringLength, string[] substrings)
+        {
+            string[] result = str.SubstringsByLength(substringLength);
+
+            Assert.True(substrings.SequenceEqual(result));
+        }
+
+        public static IEnumerable<object[]> GetSubstringsByLengthTestData()
+        {
+            yield return new object[] { "12345", 2, new string[] { "12", "23","34","45"} };
+            yield return new object[] { "12345", 4, new string[] { "1234", "2345" } };
+            yield return new object[] { "12345", 5, new string[] { "12345" } };
+            yield return new object[] { "12345", 6, new string[] { } };
+        }
+    }
+}
---
+++ Back-end/QueryWorker/Extensions/StringExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryWorker.Extensions
+{
+    public static class StringExtension
+    {
+        public static string[] SubstringsByLength(this string str, int substringLength)
+        {
+            int startIndex = 0;
+
+            if (substringLength <= 0)
+                throw new ArgumentOutOfRangeException();
+
+            var substrings = new List<string>();
+
+            while(startIndex <= str.Length - substringLength)
+            {
+                var substring = str.Substring(startIndex, substringLength);
+                substrings.Add(substring);
+                startIndex++;
+            }
+
+            return substrings.ToArray();
+        }
+    }
+}
