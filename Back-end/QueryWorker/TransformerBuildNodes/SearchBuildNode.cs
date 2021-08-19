f5224782c9af78d8a90a19bdf29fae47670fb85c
shalomich <shalo6947@gmail.com>
2021-08-20 00:30:23 +0700

    update DataTransformerFacade for Search

---
+++ Back-end/QueryWorker/TransformerBuildNodes/SearchBuildNode.cs
@@ -0,0 +1,22 @@
+﻿using QueryWorker.Args;
+using QueryWorker.TransformerBuildNodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryWorker.TransformerBuildNodes
+{
+    internal class SearchBuildNode : TransformerBuildNode
+    {
+        public SearchBuildNode(Action<string> errorHandler) : base(errorHandler)
+        {
+        }
+
+        protected override IDataTransformerArgs[] ChooseArgs(QueryArgs args)
+        {
+            return args.Searches;
+        }
+    }
+}
