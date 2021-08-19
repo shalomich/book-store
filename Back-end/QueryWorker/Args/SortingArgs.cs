c17247e8bb6c6c39ffc1fa03329720ed76cf5c0e
shalomich <shalo6947@gmail.com>
2021-08-19 12:48:54 +0700

    add all data transforemer args

---
+++ Back-end/QueryWorker/Args/SortingArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryWorker.Args
+{
+    public record SortingArgs : IDataTransformerArgs
+    {
+        [Required]
+        public string PropertyName { init; get; }
+        public bool IsAscending { init; get; } = true;
+
+        public override string ToString()
+        {
+            return $"Sorting(propertyName: {PropertyName}," +
+                $"isAscending: {IsAscending})";
+        }
+    }
+}
