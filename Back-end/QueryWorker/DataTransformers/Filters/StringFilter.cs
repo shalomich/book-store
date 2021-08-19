68a201b61cf4bc7f1470d37837d5443633c8170a
shalomich <shalo6947@gmail.com>
2021-08-19 12:46:34 +0700

    add all data transformers

---
+++ Back-end/QueryWorker/DataTransformers/Filters/StringFilter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryWorker.DataTransformers.Filters
+{
+    internal class StringFilter<T> : Filter<T, string> where T : class
+    {
+        private FilterСomparison _comparison = FilterСomparison.Equal;
+        public StringFilter(Expression<Func<T, string>> propertySelector) : base(propertySelector)
+        {
+        }
+
+        public override FilterСomparison Comparison
+        {
+            set
+            {
+                if (value != FilterСomparison.Equal)
+                    throw new ArgumentException();
+                _comparison = value;
+            }
+            get
+            {
+                return _comparison;
+            }
+        }
+
+        protected override Expression<Func<string, bool>> ChooseComparer(string comparedValue, FilterСomparison comparison)
+        {
+            return value => value == comparedValue;   
+        }
+    }
+}
---
+++ Back-end/QueryWorker/DataTransformers/Sorting.cs
@@ -0,0 +1,27 @@
+﻿
+using QueryWorker.Extensions;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace QueryWorker.DataTransformers
+{
+
+    internal class Sorting<T> : IDataTransformer<T> where T : class
+    {
+        private readonly Expression<Func<T, object>> _propertySelector;
+        public bool IsAscending { set; get; } = true;
+        public Sorting(Expression<Func<T, object>> propertySelector)
+        {
+            _propertySelector = propertySelector ?? throw new ArgumentNullException(nameof(propertySelector));
+        }
+
+        public IQueryable<T> Transform(IQueryable<T> query)
+        {
+            query =  IsAscending == true ? query.AppendOrderBy(_propertySelector).AsQueryable() 
+                : query.AppendOrderByDescending(_propertySelector);
+
+            return query;
+        }
+    }
+}
