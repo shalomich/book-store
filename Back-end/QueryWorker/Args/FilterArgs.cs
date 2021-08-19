106a7a76eb1293e9a76f2e47cdf45aebe824ddee
shalomich <shalo6947@gmail.com>
2021-08-19 12:48:54 +0700

    add all data transforemer args

---
+++ Back-end/QueryWorker/Args/FilterArgs.cs
@@ -0,0 +1,25 @@
+﻿
+using QueryWorker.DataTransformers.Filters;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace QueryWorker.Args
+{
+    public record FilterArgs : IDataTransformerArgs
+    {
+        [Required]
+        public string PropertyName { init; get; }
+        
+        [Required]
+        public string ComparedValue { init; get; }
+
+        public FilterСomparison Comparison { init; get; } = FilterСomparison.Equal;
+
+        public override string ToString()
+        {
+            return $"Filter(propertyName: {PropertyName}, " +
+                $"comparedValue: {ComparedValue}," +
+                $"comparison: {Comparison})";
+        }
+    }
+}
