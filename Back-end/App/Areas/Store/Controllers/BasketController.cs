eede0c21b3b1df55e6db56965d28bcc3879fafca
shalomich <shalo6947@gmail.com>
2021-08-23 15:53:24 +0700

    create class BasketController

---
+++ Back-end/App/Areas/Store/Controllers/BasketController.cs
@@ -0,0 +1,50 @@
+﻿using App.Areas.Store.ViewModels.Basket;
+using App.Entities;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static App.Areas.Common.RequestHandlers.CreateHandler;
+using static App.Areas.Common.RequestHandlers.DeleteHandler;
+
+namespace App.Areas.Store.Controllers
+{
+    public class BasketController : UserController
+    {
+        private IMapper Mapper { get; }
+        public BasketController(IMediator mediator, IMapper mapper) : base(mediator)
+        {
+            Mapper = mapper;
+        }
+
+        private async Task<Basket> GetUserBasket()
+        {
+            var user = await GetAuthorizedUser();
+
+            var basket = user.Basket;
+
+            return basket ?? (Basket) await Mediator.Send(new CreateCommand(new Basket() { User = user}));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<BasketDto>> Get()
+        {
+            var basket = await GetUserBasket();
+
+            return Mapper.Map<BasketDto>(basket);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<BasketDto>> Delete()
+        {
+            var basket = await GetUserBasket();
+
+            await Mediator.Send(new DeleteCommand(basket));
+
+            return NoContent();
+        }
+    }
+}
---
+++ Back-end/App/Areas/Store/ViewModels/Basket/BasketProductDto.cs
@@ -0,0 +1,16 @@
+﻿using App.Areas.Common.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Areas.Store.ViewModels.Basket
+{
+    public record BasketProductDto
+    {
+        public string Name { init; get; }
+        public int Cost { init; get; }
+        public int Quantity { init; get; }
+        public ImageDto TitleImage {init; get;}
+    }
+}
