f8aba2e7140f32395daf6a6791981f0a2c7a364e
Gevorg17 <ggevorgyan017@gmail.com>
2021-08-25 12:13:45 +0700

    fix: eslint fix #62

--- Front-end/administration-dashboard/src/app/app-routing.module.ts
+++ Front-end/administration-dashboard/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MainPageComponent } from './main-page/main-page.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: 'products', component: MainPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
