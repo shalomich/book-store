98acc48fc8710c10e7e299b6547fc17879a90ca6
Gevorg17 <ggevorgyan017@gmail.com>
2021-08-25 11:50:36 +0700

    feat: set up linter #62

--- Front-end/administration-dashboard/src/app/app.module.ts
+++ Front-end/administration-dashboard/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
