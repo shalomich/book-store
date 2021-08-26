ee611e723b5d0d9abe73904105ef86bf98848601
Gevorg17 <ggevorgyan017@gmail.com>
2021-08-26 14:07:49 +0700

    feat: add entity list item component #62

--- Front-end/administration-dashboard/src/app/app.module.ts
+++ Front-end/administration-dashboard/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { AppRoutingModule } from './app-routing.module';
 import { EntityPageComponent } from './entity-page/entity-page.component';
+import { EntityListItemComponent } from './entity-page/entity-list-item/entity-list-item.component';
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
     EntityPageComponent,
+    EntityListItemComponent,
   ],
   imports: [
     BrowserModule,
