5eaa9b58d08e5382c4d053a329a7d55ff21a0f02
shalomich <shalo6947@gmail.com>
2021-10-11 20:03:01 +0700

    Add columns Width and Height in table Image

---
+++ Back-end/BookStore.Persistance/Migrations/20211011125033_AddColumnWidthAndHeightInTableImage.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace App.Migrations
+{
+    public partial class AddColumnWidthAndHeightInTableImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BasketId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Height",
+                table: "Images",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Width",
+                table: "Images",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "21e9ec8a-bfcb-4cd0-9b91-937b21cd9953");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "645218bf-9cb2-4d49-b904-c2c0fc97e884");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Height",
+                table: "Images");
+
+            migrationBuilder.DropColumn(
+                name: "Width",
+                table: "Images");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BasketId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "a11e7fd6-a1be-449e-be0c-dd7ad7efb2a2");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "86f82db5-1203-4422-a1f1-f09fb6a753c6");
+        }
+    }
+}
