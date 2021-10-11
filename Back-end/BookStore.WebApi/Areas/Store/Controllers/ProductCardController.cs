43b4095e1bbe6cf260d4009ae2ee78c28a58bb49
shalomich <shalo6947@gmail.com>
2021-10-11 23:35:05 +0700

    fix bug with uncorrect pageCount

--- Back-end/BookStore.WebApi/Areas/Store/Controllers/ProductCardController.cs
+++ Back-end/BookStore.WebApi/Areas/Store/Controllers/ProductCardController.cs
@@ -66,9 +66,11 @@ namespace BookStore.WebApi.Areas.Store.Controllers
         [HttpHead]
         public async Task GetPaggingMetadata([FromQuery] QueryTransformArgs args)
         {
+            int dataCount = QueryBuilder.Build().Count();
+
             QueryBuilder.AddDataTransformation(args);
 
-            var metadata = await Mediator.Send(new GetMetadataQuery(args.Pagging, QueryBuilder));
+            var metadata = await Mediator.Send(new GetMetadataQuery(dataCount, args.Pagging, QueryBuilder));
             
             HttpContext.Response.Headers.Add(metadata);
         }
