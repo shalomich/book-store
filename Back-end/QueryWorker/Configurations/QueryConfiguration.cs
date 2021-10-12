e558606f611763b46603b541ea21f29f2bdf207f
shalomich <shalo6947@gmail.com>
2021-10-12 09:08:22 +0700

    Fix params naming

--- Back-end/QueryWorker/Configurations/QueryConfiguration.cs
+++ Back-end/QueryWorker/Configurations/QueryConfiguration.cs
@@ -14,8 +14,6 @@ namespace QueryWorker.Configurations
 {
     public abstract class QueryConfiguration<TConfigured> where TConfigured : class
     {
-        private const int MaxPageNumber = 60;
-
         private Pagging<TConfigured> _pagging;
 
         private readonly Dictionary<string, DataTransformer<TConfigured>> _sortings = new Dictionary<string, DataTransformer<TConfigured>>();
@@ -25,40 +23,37 @@ namespace QueryWorker.Configurations
         internal DataTransformer<TConfigured> GetSorting(string propertyKey) => _sortings[propertyKey];
         internal DataTransformer<TConfigured> GetFilter(string propertyKey) => _filters[propertyKey];
         internal DataTransformer<TConfigured> GetSearch(string propertyKey) => _searches[propertyKey];
-        internal DataTransformer<TConfigured> GetPagging() => _pagging ?? new Pagging<TConfigured>(MaxPageNumber);
+        internal DataTransformer<TConfigured> GetPagging() => _pagging ?? new Pagging<TConfigured>();
 
         protected void CreateSorting(string propertyKey,Expression<Func<TConfigured,object>> propertySelector)
         {
             var sorting = new Sorting<TConfigured>(propertySelector);
 
-            _sortings.Add(propertyKey, sorting);
+            _sortings.Add(propertyKey.ToLowFirstLetter(), sorting);
         }
 
         protected void CreateFilter(string propertyKey,Expression<Func<TConfigured, string>> propertySelector)
         {
-            _filters.Add(propertyKey, new StringFilter<TConfigured>(propertySelector));
+            _filters.Add(propertyKey.ToLowFirstLetter(), new StringFilter<TConfigured>(propertySelector));
         }
 
         protected void CreateFilter(string propertyKey,Expression<Func<TConfigured, double>> propertySelector)
         {
-            _filters.Add(propertyKey, new NumberFilter<TConfigured>(propertySelector));
+            _filters.Add(propertyKey.ToLowFirstLetter(), new NumberFilter<TConfigured>(propertySelector));
         }
 
         protected void CreateFilter(string propertyKey, Expression<Func<TConfigured, IEnumerable<int>>> propertySelector)
         {
-            _filters.Add(propertyKey, new CollectionFilter<TConfigured>(propertySelector));
+            _filters.Add(propertyKey.ToLowFirstLetter(), new CollectionFilter<TConfigured>(propertySelector));
         }
 
         protected void CreateSearch(string propertyKey, Expression<Func<TConfigured, string>> propertySelector)
         {
-            _searches.Add(propertyKey, new Search<TConfigured>(propertySelector));
+            _searches.Add(propertyKey.ToLowFirstLetter(), new Search<TConfigured>(propertySelector));
         }
 
         protected void CreatePagging(int maxPageNumberForConfigured)
         {
-            if (maxPageNumberForConfigured > MaxPageNumber)
-                maxPageNumberForConfigured = MaxPageNumber;
-
             _pagging = new Pagging<TConfigured>(maxPageNumberForConfigured);
         }
 
