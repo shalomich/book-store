d5e10588f8f20df0e8f172e473e9754924119d01
Gevorg17 <ggevorgyan017@gmail.com>
2021-10-23 20:08:01 +0700

    feat: add interfaces for book-dto and image #91

--- Front-end/store/src/app/app.module.ts
+++ Front-end/store/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductCardComponent } from './product-card/product-card.component';
+import { ProductInfoComponent } from './product-card/product-info/product-info.component';
+import { ProductImagesComponent } from './product-card/product-images/product-images.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductCardComponent,
+    ProductInfoComponent,
+    ProductImagesComponent
   ],
   imports: [
     BrowserModule,
