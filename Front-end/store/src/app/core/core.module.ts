07dbee5063f3a86991408263471ce00dc0dbb84d
Gevorg17 <ggevorgyan017@gmail.com>
2021-10-26 18:52:44 +0700

    feat: add images and cart blocks #91

--- Front-end/store/src/app/core/core.module.ts
+++ Front-end/store/src/app/core/core.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { Base64ToImgSrcPipe } from './pipes/base64-to-img-src.pipe';
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
+  declarations: [Base64ToImgSrcPipe],
+  imports: [CommonModule],
+  exports: [
+    Base64ToImgSrcPipe
   ]
 })
 export class CoreModule { }
--- Front-end/store/src/app/shared/product-card/add-to-cart/add-to-cart.component.ts
+++ Front-end/store/src/app/shared/product-card/add-to-cart/add-to-cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-add-to-cart',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddToCartComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  public cost: string = '';
 
-  ngOnInit(): void {
+  public constructor() { }
+
+  public ngOnInit(): void {
   }
 
 }
