f28ee88a4859636ece891ddec13bbf2d66c8e022
Gevorg17 <ggevorgyan017@gmail.com>
2021-11-02 21:07:54 +0700

    feat: add filters component #106

--- Front-end/store/src/app/shared/shared.module.ts
+++ Front-end/store/src/app/shared/shared.module.ts
@@ -1,28 +1,44 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { MatButtonModule } from '@angular/material/button';
+
+import { CoreModule } from '../core/core.module';
+
 import { ProductImagesComponent } from './product-card/product-images/product-images.component';
 import { AddToCartComponent } from './product-card/add-to-cart/add-to-cart.component';
-import {CoreModule} from '../core/core.module';
-import {MatButtonModule} from '@angular/material/button';
-import {ProductListItemComponent} from './product-list-item/product-list-item.component';
 
+
+import { ProductListItemComponent } from './product-list-item/product-list-item.component';
+import { RangeFilterComponent } from './filters/range-filter/range-filter.component';
+import { PlentyFilterComponent } from './filters/plenty-filter/plenty-filter.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
-    declarations: [
-        ProductImagesComponent,
-        AddToCartComponent,
-        ProductListItemComponent
-    ],
-    exports: [
-        ProductImagesComponent,
-        AddToCartComponent,
-        ProductListItemComponent
-    ],
-    imports: [
-        CommonModule,
-        CoreModule,
-        MatButtonModule
-    ]
+  declarations: [
+    ProductImagesComponent,
+    AddToCartComponent,
+    ProductListItemComponent,
+    RangeFilterComponent,
+    PlentyFilterComponent,
+  ],
+  exports: [
+    ProductImagesComponent,
+    AddToCartComponent,
+    ProductListItemComponent,
+    RangeFilterComponent,
+    PlentyFilterComponent,
+  ],
+  imports: [
+    CommonModule,
+    CoreModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+  ],
 })
 export class SharedModule { }
