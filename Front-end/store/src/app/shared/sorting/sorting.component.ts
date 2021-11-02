f2fd8b629553bf7d444c5b488db131c08d16d4ca
shalomich <shalo6947@gmail.com>
2021-11-02 14:48:59 +0700

    add order button to sorting component

--- Front-end/store/src/app/shared/sorting/sorting.component.ts
+++ Front-end/store/src/app/shared/sorting/sorting.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import {MatSelectChange} from "@angular/material/select";
 import {SortingOptions} from "../../core/interfaces/sorting-options";
 import {BehaviorSubject} from "rxjs";
+import {MatOptionSelectionChange} from "@angular/material/core";
 
 @Component({
   selector: 'sorting',
@@ -13,15 +14,30 @@ export class SortingComponent {
   @Input() propertyNamesWithText: Array<[string, string]> = [];
   @Input() sortingOptions$: BehaviorSubject<Array<SortingOptions>> = new BehaviorSubject<Array<SortingOptions>>([]);
 
-  public onSelectChanged(event: MatSelectChange) {
-    const propertyNames = event.value as string[];
+  private options: Array<SortingOptions> = []
 
-    this.sortingOptions$.next(propertyNames.map<SortingOptions>(name => {
-      return {
-        propertyName: name,
+  public onOptionChanged(event: MatOptionSelectionChange) {
+    const propertyName = event.source.value;
+
+    if (event.source.selected) {
+      this.options.push({
+        propertyName: propertyName,
         isAscending: true
-      }
-    }));
+      })
+    } else {
+      const removedIndex = this.options.findIndex(option => option.propertyName == propertyName);
+      this.options.splice(removedIndex, 1);
+     }
+
+    this.sortingOptions$.next(this.options);
   }
 
+  public onOrderButtonClick(propertyName: string) {
+    const option = this.options.find(option => option.propertyName == propertyName)!;
+
+    if (option) {
+      option.isAscending = !option.isAscending;
+      this.sortingOptions$.next(this.options);
+    }
+  }
 }
