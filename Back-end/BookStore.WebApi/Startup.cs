01c1ed7c4a233647e5e656f0fccbec6d300def2c
shalomich <shalo6947@gmail.com>
2021-11-03 20:03:43 +0300

    Make to return RelatedEntityDto instead IEntity

--- Back-end/BookStore.WebApi/Startup.cs
+++ Back-end/BookStore.WebApi/Startup.cs
@@ -73,7 +73,7 @@ namespace BookStore.WebApi
                 });
 
             services.AddMediatR(applicationAssembly);
-            services.AddAutoMapper(GetType());
+            services.AddAutoMapper(Assembly.GetExecutingAssembly(), applicationAssembly);
             services.AddCors();
 
             services.AddSwaggerGen();
