48de9def96dd60bf92c28201ce3ab842cba13498
shalomich <shalo6947@gmail.com>
2021-11-04 11:23:24 +0300

    Add checking title image name exists

--- Back-end/BookStore.WebApi/Areas/Dashboard/ViewModels/Forms/AlbumForm.cs
+++ Back-end/BookStore.WebApi/Areas/Dashboard/ViewModels/Forms/AlbumForm.cs
@@ -1,4 +1,5 @@
-﻿using BookStore.Domain.Entities.Products;
+﻿using BookStore.Application.Exceptions;
+using BookStore.Domain.Entities.Products;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -9,10 +10,20 @@ namespace BookStore.WebApi.Areas.Dashboard.ViewModels.Forms
 {
     public record AlbumForm
     {
+        private const string InvalidTitleImageName = "This is not image with name as album title image name";
         [Required]
         public string TitleImageName { init; get; }
 
         [Required, MinLength(Album.MinImageCount), MaxLength(Album.MaxImageCount)]
         public ISet<ImageForm> Images { init; get; }
+
+        public AlbumForm(string titleImageName, ISet<ImageForm> images)
+        {
+            if (images.Any(image => image.Name == titleImageName) == false)
+                throw new BadRequestException(InvalidTitleImageName);
+
+            TitleImageName = titleImageName;
+            Images = images;
+        }
     }
 }
