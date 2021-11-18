f723890c9bbad6eccd5fd8d9818b392cb1bf6353
shalomich <shalo6947@gmail.com>
2021-11-18 17:56:55 +0300

    create specification filter

---
+++ Back-end/BookStore.Application/DbQueryConfigs/Specifications/AvailableInStockSpecification.cs
@@ -0,0 +1,19 @@
+﻿using Abp.Specifications;
+using BookStore.Domain.Entities.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.DbQueryConfigs.Specifications
+{
+    internal class AvailableInStockSpecification<T> : Specification<T> where T : Product
+    {
+        public override Expression<Func<T, bool>> ToExpression()
+        {
+            return product => product.Quantity != 0;
+        }
+    }
+}
