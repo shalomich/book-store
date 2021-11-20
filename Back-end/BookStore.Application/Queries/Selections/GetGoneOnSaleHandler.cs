9aaaa8ab26774f7e7d8f642a881f582ab116dafa
shalomich <shalo6947@gmail.com>
2021-11-20 17:02:36 +0300

    create query GetGoneOnSaleQuery

---
+++ Back-end/BookStore.Application/Queries/Selections/GetGoneOnSaleHandler.cs
@@ -0,0 +1,27 @@
+﻿using BookStore.Application.DbQueryConfigs.Specifications;
+using BookStore.Application.Services.DbQueryBuilders;
+using BookStore.Domain.Entities.Books;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.Queries.Selections
+{
+    public record GetGoneOnSaleQuery(DbFormEntityQueryBuilder<Book> Builder) : IRequest<IEnumerable<Book>>;
+    internal class GetGoneOnSaleHandler : IRequestHandler<GetGoneOnSaleQuery, IEnumerable<Book>>
+    {
+        public async Task<IEnumerable<Book>> Handle(GetGoneOnSaleQuery request, CancellationToken cancellationToken)
+        {
+            return await request.Builder
+                .AddSpecification(new GoneOnSaleSpecification())
+                .Build()
+                .OrderByDescending(book => book.AddingDate)
+                .ToListAsync();
+        }
+    }
+}
