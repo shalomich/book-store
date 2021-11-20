67b95d73755375e0c6a4c61ee107dabf0785b9e1
shalomich <shalo6947@gmail.com>
2021-11-20 15:24:27 +0300

    add method GetNovelty in SelectionController

---
+++ Back-end/BookStore.WebApi/Areas/Store/Controllers/SelectionController.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using BookStore.Application.Queries.Selections;
+using BookStore.Application.Services.DbQueryBuilders;
+using BookStore.Domain.Entities.Books;
+using BookStore.WebApi.Areas.Store.ViewModels.Cards;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using QueryWorker.Args;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.WebApi.Areas.Store.Controllers
+{
+    [Route("[area]/selection/book")]
+    public class SelectionController : StoreController
+    {
+        private IMediator Mediator { get; }
+        private IMapper Mapper { get; }
+        private DbFormEntityQueryBuilder<Book> Builder { get; }
+
+        public SelectionController(IMediator mediator, IMapper mapper, DbFormEntityQueryBuilder<Book> builder)
+        {
+            Mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            Mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            Builder = builder ?? throw new ArgumentNullException(nameof(builder));
+        }
+
+        [HttpGet("novelty")]
+        public async Task<ActionResult<ProductCard[]>> GetNovelty([FromQuery] PaggingArgs pagging) 
+        {
+            Builder.AddPagging(pagging);
+
+            return (await Mediator.Send(new GetNoveltyQuery(Builder)))
+                .Select(book => Mapper.Map<ProductCard>(book))
+                .ToArray();
+        }
+    }
+}
