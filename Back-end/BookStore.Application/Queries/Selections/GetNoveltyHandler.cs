f032817ac570b7478ee3d7cc4351fe6b7c199348
shalomich <shalo6947@gmail.com>
2021-11-20 15:23:13 +0300

    create query GetNoveltyQuery

---
+++ Back-end/BookStore.Application/Queries/Selections/GetNoveltyHandler.cs
@@ -0,0 +1,27 @@
+﻿using BookStore.Application.DbQueryConfigs.Specifications;
+using BookStore.Application.Services.DbQueryBuilders;
+using BookStore.Domain.Entities.Books;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.Queries.Selections
+{
+    public record GetNoveltyQuery(DbFormEntityQueryBuilder<Book> Builder) : IRequest<IEnumerable<Book>>;
+    internal class GetNoveltyHandler : IRequestHandler<GetNoveltyQuery, IEnumerable<Book>>
+    {
+        public async Task<IEnumerable<Book>> Handle(GetNoveltyQuery request, CancellationToken cancellationToken)
+        {
+            return await request.Builder
+                .AddSpecification(new NoveltySpecification())
+                .Build()
+                .OrderByDescending(book => book.AddingDate)
+                .ToListAsync();
+        }
+    }
+}
