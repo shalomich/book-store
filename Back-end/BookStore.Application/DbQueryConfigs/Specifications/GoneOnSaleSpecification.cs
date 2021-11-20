cf29de81cd45345a8cb5f9ae5c0cb1681e161b4a
shalomich <shalo6947@gmail.com>
2021-11-20 17:02:36 +0300

    create query GetGoneOnSaleQuery

---
+++ Back-end/BookStore.Application/DbQueryConfigs/Specifications/GoneOnSaleSpecification.cs
@@ -0,0 +1,21 @@
+﻿using Abp.Specifications;
+using BookStore.Domain.Entities.Books;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.DbQueryConfigs.Specifications
+{
+    internal class GoneOnSaleSpecification : Specification<Book>
+    {
+        private const int GoneOnSaleDaysCount = 7;
+        public override Expression<Func<Book, bool>> ToExpression()
+        {
+            return book => EF.Functions.DateDiffDay(book.AddingDate, DateTime.Now) < GoneOnSaleDaysCount; 
+        }
+    }
+}
