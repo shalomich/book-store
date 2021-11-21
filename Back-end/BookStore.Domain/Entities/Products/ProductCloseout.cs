218ad7b66c5c61cedc91cd90a880d6daff56da33
shalomich <shalo6947@gmail.com>
2021-11-21 13:59:18 +0300

    create class ProductCloseout

---
+++ Back-end/BookStore.Domain/Entities/Products/ProductCloseout.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Domain.Entities.Products
+{
+    public class ProductCloseout : IEntity
+    {
+        private DateTime? _replenishmentDate;
+        public int Id { set; get; }
+        public DateTime Date { set; get; }
+        public DateTime? ReplenishmentDate 
+        { 
+            set
+            {
+                if (value < Date)
+                    throw new ArgumentOutOfRangeException();
+
+                _replenishmentDate = value;
+            }
+            get
+            {
+                return _replenishmentDate;
+            }
+        }
+        public Product Product { set; get; }
+        public int ProductId { set; get; }
+    }
+}
