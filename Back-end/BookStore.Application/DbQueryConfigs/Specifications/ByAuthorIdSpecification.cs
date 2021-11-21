ec9a06027e70a625f781775a53c0f6e1e364a93c
shalomich <shalo6947@gmail.com>
2021-11-21 15:31:38 +0300

    create query GetByRandomAuthorHandler

---
+++ Back-end/BookStore.Application/DbQueryConfigs/Specifications/ByAuthorIdSpecification.cs
@@ -0,0 +1,26 @@
+﻿using Abp.Specifications;
+using BookStore.Domain.Entities.Books;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.DbQueryConfigs.Specifications
+{
+    internal class ByAuthorIdSpecification : Specification<Book>
+    {
+        private int AuthorId { get; }
+
+        public ByAuthorIdSpecification(int authorId)
+        {
+            AuthorId = authorId;
+        }
+
+        public override Expression<Func<Book, bool>> ToExpression()
+        {
+            return book => book.AuthorId == AuthorId;
+        }
+    }
+}
