8ac8759e256895c7cbda021fff16d7d00b9b28a9
shalomich <shalo6947@gmail.com>
2021-11-21 14:00:02 +0300

    create query GetBackOnSaleHandler

---
+++ Back-end/BookStore.Application/Queries/Selections/GetBackOnSaleHandler.cs
@@ -0,0 +1,28 @@
+﻿using BookStore.Application.DbQueryConfigs.Specifications;
+using BookStore.Application.Services.DbQueryBuilders;
+using BookStore.Domain.Entities.Books;
+using BookStore.Domain.Entities.Products;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.Queries.Selections
+{
+    public record GetBackOnSaleQuery(DbFormEntityQueryBuilder<Book> Builder) : IRequest<IEnumerable<Book>>;
+    internal class GetBackOnSaleHandler : IRequestHandler<GetBackOnSaleQuery, IEnumerable<Book>>
+    {
+        public async Task<IEnumerable<Book>> Handle(GetBackOnSaleQuery request, CancellationToken cancellationToken)
+        {
+            return await request.Builder
+                .AddSpecification(new BackOnSaleSpecification<Book>())
+                .Build()
+                .OrderBy(book => EF.Functions.DateDiffDay(book.ProductCloseout.ReplenishmentDate, DateTime.Now))
+                .ToListAsync();
+        }
+    }
+}
