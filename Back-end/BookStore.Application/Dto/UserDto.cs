44cb8fb2ca22423e6323ff0df5e639bbbc62e61d
shalomich <shalo6947@gmail.com>
2021-12-21 17:09:25 +0700

    Add enpoints for user profile

---
+++ Back-end/BookStore.Application/Dto/UserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.Dto
+{
+    public record UserDto
+    {
+        public string UserName { init; get; }
+        public string Email { init; get; }
+        public string PhoneNumber { init; get; }
+    }
+}
