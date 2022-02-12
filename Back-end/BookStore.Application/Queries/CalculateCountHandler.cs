705e177ef79754e366ea27311af627377e20b0c2
shalomich <shalomich4769@gmail.com>
2022-02-12 14:45:26 +0700

    Create calculate count query

---
+++ Back-end/BookStore.Application/Queries/CalculateCountHandler.cs
@@ -0,0 +1,25 @@
+﻿using BookStore.Application.Services.DbQueryBuilders;
+using BookStore.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.Queries
+{
+    public record CalculateCountQuery(IDbQueryBuilder<IEntity> QueryBuilder) : IRequest<int>;
+    internal class CalculateCountHandler : IRequestHandler<CalculateCountQuery, int>
+    {
+        public Task<int> Handle(CalculateCountQuery request, CancellationToken cancellationToken)
+        {
+            return request.QueryBuilder
+                .Build()
+                .CountAsync();
+        }
+    }
+
+}
