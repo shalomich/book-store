a8b521b9e1ceb057034c3065655322baf282fa48
shalomich <shalomich4769@gmail.com>
2022-02-16 00:08:01 +0700

    Create request for search hints getting

---
+++ Back-end/BookStore.Application/Dto/SearchHintsDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.Dto
+{
+    public record SearchHintsDto
+    {
+        public string[] Books { init; get; }
+        public string[] Authors { init; get; }
+        public string[] Publishers { init; get; }
+    }
+}
