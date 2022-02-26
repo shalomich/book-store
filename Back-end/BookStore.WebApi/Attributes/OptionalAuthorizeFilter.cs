1e2acaec1c6c656c8357a02d9dc78977c7be507e
shalomich <shalomich4769@gmail.com>
2022-02-27 00:40:34 +0700

    Create optional authorize filter.

---
+++ Back-end/BookStore.WebApi/Attributes/OptionalAuthorizeFilter.cs
@@ -0,0 +1,45 @@
+﻿using BookStore.Application.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Security.Claims;
+
+namespace BookStore.WebApi.Attributes
+{
+    public class OptionalAuthorizeFilter : IAuthorizationFilter
+    {
+        private JwtParser JwtParser { get;}
+
+        public OptionalAuthorizeFilter(JwtParser jwtParser)
+        {
+            JwtParser = jwtParser;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var httpContext = context.HttpContext;
+
+            if (httpContext.User.Identity.IsAuthenticated)
+                return;
+
+            if (!context.HttpContext.Request.Headers
+                .TryGetValue("Authorization", out StringValues authorizationHeader))
+                return;
+
+            string token = authorizationHeader
+                .ToString()
+                .Replace("Bearer ", "");
+
+            int userId = JwtParser.FromToken(token);
+
+            var identity = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(nameof(userId), userId.ToString())
+            }, "Custom");
+
+            httpContext.User = new ClaimsPrincipal(identity);
+        }
+    }
+}
