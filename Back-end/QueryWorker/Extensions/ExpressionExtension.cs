2d6f9be3b4b0b3f0e27b3d97dbd90d7568661f19
shalomich <shalomich4769@gmail.com>
2022-03-09 19:48:22 +0700

    Fix plenty filters.

--- Back-end/QueryWorker/Extensions/ExpressionExtension.cs
+++ Back-end/QueryWorker/Extensions/ExpressionExtension.cs
@@ -10,6 +10,14 @@ namespace QueryWorker.Extensions
 {
     internal static class ExpressionExtension
     {
+        public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expr1,
+            Expression<Func<T, bool>> expr2)
+        {
+            var invokedExpr = Expression.Invoke(expr2, expr1.Parameters.Cast<Expression>());
+            return Expression.Lambda<Func<T, bool>>(
+                Expression.AndAlso(expr1.Body, invokedExpr), expr1.Parameters);
+        }
+
         public static Expression ReplaceParameter(this Expression expression,
             ParameterExpression toReplace,
             Expression newExpression)
