ca3ebfc65652ed659ed0fffece2561aee3d48c29
Gevorg17 <ggevorgyan017@gmail.com>
2022-03-12 22:23:34 +0700

    feat: add basket info block #145

--- Front-end/store/src/app/basket-page/basket-info-block/basket-info-block.component.ts
+++ Front-end/store/src/app/basket-page/basket-info-block/basket-info-block.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { BasketProduct } from '../../core/interfaces/basket-product';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-basket-info-block',
   templateUrl: './basket-info-block.component.html',
-  styleUrls: ['./basket-info-block.component.css']
+  styleUrls: ['./basket-info-block.component.css'],
 })
 export class BasketInfoBlockComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  public products: Observable<BasketProduct[]> = new Observable<BasketProduct[]>();
 
-  ngOnInit(): void {
+  public totalCost = 0;
+
+  public totalAmount = 0;
+
+  constructor() {
+  }
+
+  public ngOnInit(): void {
+    this.products.subscribe(products => {
+      this.totalAmount = products.reduce((sum, a) => sum + a.quantity, 0);
+
+      this.totalCost = products.reduce((sum, a) => sum + (a.cost * a.quantity), 0);
+    });
   }
 
 }
