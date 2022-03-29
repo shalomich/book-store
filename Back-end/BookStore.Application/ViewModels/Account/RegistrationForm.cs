1e963fd4c17757a61bdc905abffe5dc21f87e012
shalomich <shalomich4769@gmail.com>
2022-03-29 19:58:57 +0700

    Fix bug with login.

---
+++ Back-end/BookStore.Application/ViewModels/Account/RegistrationForm.cs
@@ -0,0 +1,18 @@
+﻿using DataAnnotationsExtensions;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Application.ViewModels.Account;
+
+public record RegistrationForm
+{
+    [Required]
+    [Email]
+    public string Email { init; get; }
+
+    [Required]
+    public string UserName { init; get; }
+
+    [Required]
+    public string Password { init; get; }
+}
+
