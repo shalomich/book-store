8a2572ded093e6920e84a172e90a6f2003af05e0
shalomich <shalomich4769@gmail.com>
2022-03-29 20:12:48 +0700

    Replace user dto with user profile form for updating.

---
+++ Back-end/BookStore.Application/ViewModels/Profile/UserProfileForm.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Application.ViewModels.Profile;
+
+public record UserProfileForm
+{
+    [Required]
+    public string FirstName { init; get; }
+    public string LastName { init; get; }
+    public string PhoneNumber { init; get; }
+    public string Address { init; get; }
+}
+
