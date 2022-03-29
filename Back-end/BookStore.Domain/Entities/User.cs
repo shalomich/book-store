a95fb7406b9c13d2f1cd6181e051303b09b90f76
shalomich <shalomich4769@gmail.com>
2022-03-29 20:03:23 +0700

    Add property FirstName and LastName to User.

--- Back-end/BookStore.Domain/Entities/User.cs
+++ Back-end/BookStore.Domain/Entities/User.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,9 @@ namespace BookStore.Domain.Entities
 {
     public class User : IdentityUser<int>, IEntity
     {
+        [Required]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Address { get; set; }
         public ISet<BasketProduct> BasketProducts { get; set; }
         public IEnumerable<Order> Orders { get; set; }
