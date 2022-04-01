0b1e2ee8d05d5ed54598d9d4c696bfb718828475
Gevorg17 <ggevorgyan017@gmail.com>
2022-04-01 20:48:42 +0700

    fix: resolve token problem #190

--- Front-end/store/src/app/core/core.module.ts
+++ Front-end/store/src/app/core/core.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { JwtModule } from '@auth0/angular-jwt';
+
 import { Base64ToImgSrcPipe } from './pipes/base64-to-img-src.pipe';
 
 
 @NgModule({
   declarations: [Base64ToImgSrcPipe],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: () => localStorage.getItem('token'),
+      },
+    }),
+  ],
   exports: [Base64ToImgSrcPipe],
 })
 export class CoreModule { }
