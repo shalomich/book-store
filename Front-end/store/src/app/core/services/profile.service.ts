510c769b3a516fc3afe78b6a3796f968911f5543
Gevorg17 <ggevorgyan017@gmail.com>
2022-04-06 22:07:21 +0700

    feat: add profile page #195

--- Front-end/store/src/app/core/services/profile.service.ts
+++ Front-end/store/src/app/core/services/profile.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 import { map } from 'rxjs/operators';
 
@@ -8,25 +8,37 @@ import { UserProfile } from '../models/user-profile';
 import { PROFILE_URL } from '../utils/values';
 import { UserProfileDto } from '../DTOs/user-profile-dto';
 import { UserProfileMapper } from '../mappers/user-profile.mapper';
-import {AuthorizationDataProvider} from './authorization-data.provider';
+
+import { AuthorizationDataProvider } from './authorization-data.provider';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProfileService {
 
-  private userProfile: BehaviorSubject<UserProfile> = new BehaviorSubject<UserProfile>({} as UserProfile);
+  public userProfile: Observable<UserProfile> = new Observable<UserProfile>();
 
-  constructor(private readonly http: HttpClient, private readonly userProfileMapper: UserProfileMapper, private readonly authorizationDataProvider: AuthorizationDataProvider,) { }
+  constructor(
+    private readonly http: HttpClient,
+    private readonly userProfileMapper: UserProfileMapper,
+    private readonly authorizationDataProvider: AuthorizationDataProvider,
+  ) { }
 
-  public getUserProfile() {
+  public getUserProfile(): void {
     const headers = {
       Authorization: `Bearer ${this.authorizationDataProvider.token.value}`,
     };
 
-    this.http.get<UserProfileDto>(PROFILE_URL, { headers }).pipe(
-      map(profile => this.userProfile.next(this.userProfileMapper.fromDto(profile))),
-    )
-      .subscribe();
+    this.userProfile = this.http.get<UserProfileDto>(PROFILE_URL, { headers }).pipe(
+      map(profile => this.userProfileMapper.fromDto(profile)),
+    );
+  }
+
+  public saveProfileChanges(profile: UserProfile): void {
+    const headers = {
+      Authorization: `Bearer ${this.authorizationDataProvider.token.value}`,
+    };
+
+    this.http.put<void>(PROFILE_URL, profile, { headers }).subscribe();
   }
 }
