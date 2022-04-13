2ccc603726b015220134044cb4889cb29ac23d34
shalomich <shalomich4769@gmail.com>
2022-04-13 22:33:23 +0700

    Create endpoint for battle info getting.

---
+++ Back-end/BookStore.Application/Queries/Battle/GetBattleInfo/BattleInfoDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BookStore.Application.Queries.Battle.GetBattleInfo;
+public record BattleInfoDto
+{
+    public BattleBookPreviewDto FirstBattleBook { get; init; }
+    public BattleBookPreviewDto SecondBattleBook { get; init; }
+    public DateTimeOffset EndDate { get; init; }
+}
+
---
+++ Back-end/BookStore.Application/Queries/Battle/GetBattleInfo/GetBattleInfoHandler.cs
@@ -0,0 +1,79 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using BookStore.Application.Services;
+using BookStore.Domain.Entities.Battles;
+using BookStore.Persistance;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.Queries.Battle.GetBattleInfo;
+
+public record GetBattleInfoQuery() : IRequest<BattleInfoDto>;
+internal class GetBattleInfoHandler : IRequestHandler<GetBattleInfoQuery, BattleInfoDto>
+{
+    private ApplicationContext Context { get; }
+    private IMapper Mapper { get; }
+    private LoggedUserAccessor LoggedUserAccessor { get; }
+
+    public GetBattleInfoHandler(ApplicationContext context, IMapper mapper, LoggedUserAccessor loggedUserAccessor)
+    {
+        Context = context;
+        Mapper = mapper;
+        LoggedUserAccessor = loggedUserAccessor;
+    }
+
+    public async Task<BattleInfoDto> Handle(GetBattleInfoQuery request, CancellationToken cancellationToken)
+    {
+        var currentBattle = Context.Battles
+            .Where(battle => battle.IsActive);
+
+        var battleInfo = await currentBattle
+            .ProjectTo<BattleInfoDto>(Mapper.ConfigurationProvider)
+            .SingleAsync(cancellationToken);
+
+        int currentUserId = LoggedUserAccessor.GetCurrentUserId();
+
+        var currentUserVote = await currentBattle
+            .SelectMany(battle => battle.BattleBooks)
+            .SelectMany(battle => battle.Votes)
+            .SingleOrDefaultAsync(vote => vote.UserId == currentUserId, cancellationToken);
+
+        battleInfo = SetVotedStatus(battleInfo, currentUserVote);
+
+        return battleInfo;
+    }
+
+    private BattleInfoDto SetVotedStatus(BattleInfoDto battleInfo, Vote currentUserVote)
+    {
+        if (currentUserVote != null)
+        {
+            if (currentUserVote.BattleBookId == battleInfo.FirstBattleBook.BattleBookId)
+            {
+                return battleInfo with
+                {
+                    FirstBattleBook = battleInfo.FirstBattleBook with
+                    {
+                        VotedByCurrentUser = true
+                    }
+                };
+            }
+            else if (currentUserVote.BattleBookId == battleInfo.SecondBattleBook.BattleBookId)
+            {
+                return battleInfo with
+                {
+                    SecondBattleBook = battleInfo.SecondBattleBook with
+                    {
+                        VotedByCurrentUser = true
+                    }
+                };
+            }
+        }
+
+        return battleInfo;
+    }
+}
+
