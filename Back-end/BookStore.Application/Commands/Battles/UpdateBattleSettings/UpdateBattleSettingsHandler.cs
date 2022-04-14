222e46d17e2d85914120eefb6671b79d9861a8c5
shalomich <shalomich4769@gmail.com>
2022-04-14 19:20:58 +0700

    Create BattleSettingsProvider.

---
+++ Back-end/BookStore.Application/Commands/Battles/UpdateBattleSettings/UpdateBattleSettingsHandler.cs
@@ -0,0 +1,29 @@
+﻿using BookStore.Application.Queries.Battle.GetBattleSettings;
+using BookStore.Application.Services;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.Commands.Battles.UpdateBattleSettings;
+
+public record UpdateBattleSettingsCommand(BattleSettings BattleSettings) : IRequest;
+internal class UpdateBattleSettingsHandler : AsyncRequestHandler<UpdateBattleSettingsCommand>
+{
+    private BattleSettingsProvider BattleSettingsProvider { get; }
+
+    public UpdateBattleSettingsHandler(BattleSettingsProvider battleSettingsProvider)
+    {
+        BattleSettingsProvider = battleSettingsProvider;
+    }
+
+    protected override Task Handle(UpdateBattleSettingsCommand request, CancellationToken cancellationToken)
+    {
+        BattleSettingsProvider.UpdateBattleSettings(request.BattleSettings);
+
+        return Task.CompletedTask;
+    }
+}
+
---
+++ Back-end/BookStore.Application/Services/BattleSettingsProvider.cs
@@ -0,0 +1,43 @@
+﻿using BookStore.Application.Queries.Battle.GetBattleSettings;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace BookStore.Application.Services;
+internal class BattleSettingsProvider
+{
+    private readonly string _battleSettingsFilePath;
+    public BattleSettingsProvider(IConfiguration configuration)
+    {
+        string contentRootPath = configuration["ContentRootPath"];
+
+        _battleSettingsFilePath = Path.Combine(contentRootPath, "battlesettings.json");
+    }
+    public BattleSettings GetBattleSettings()
+    {
+        if (!File.Exists(_battleSettingsFilePath))
+        {
+            var battleSettings = new BattleSettings();
+
+            UpdateBattleSettings(battleSettings);
+
+            return battleSettings;
+        }
+        else
+        {
+            var battleSettingsString = File.ReadAllText(_battleSettingsFilePath);
+
+            return JsonConvert.DeserializeObject<BattleSettings>(battleSettingsString);
+        }
+    }
+
+    public void UpdateBattleSettings(BattleSettings battleSettings)
+    {
+        using var streamWriter = File.CreateText(_battleSettingsFilePath); ;
+
+        var battleSettingsString = JsonConvert.SerializeObject(battleSettings, Formatting.Indented);
+
+        streamWriter.Write(battleSettingsString);
+    }
+}
+
