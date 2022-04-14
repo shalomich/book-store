31bb961b9335eb0165718fb7f832e901d20c1fae
Roman Simernin <shalo6947@gmail.com>
2022-04-14 10:11:09 +0700

    Create endpoint for spending voting points.

---
+++ Back-end/BookStore.Application/Commands/Battle/SpendVotingPointsHandler.cs
@@ -0,0 +1,70 @@
+﻿
+using BookStore.Application.Exceptions;
+using BookStore.Application.Services;
+using BookStore.Persistance;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.Commands.Battle;
+
+public record SpendVotingPointsCommand(int VotingPointCount) : IRequest;
+internal class SpendVotingPointsHandler : AsyncRequestHandler<SpendVotingPointsCommand>
+{
+    private ApplicationContext Context { get; }
+    private LoggedUserAccessor LoggedUserAccessor { get; }
+
+    public SpendVotingPointsHandler(ApplicationContext context, LoggedUserAccessor loggedUserAccessor)
+    {
+        Context = context;
+        LoggedUserAccessor = loggedUserAccessor;
+    }
+
+    protected override async Task Handle(SpendVotingPointsCommand request, CancellationToken cancellationToken)
+    {
+        await Validate(request, cancellationToken);
+
+        int currentUserId = LoggedUserAccessor.GetCurrentUserId();
+
+        var currentUser = await Context.Users
+            .SingleAsync(user => user.Id == currentUserId, cancellationToken);
+
+        var currentUserVote = await Context.Votes
+            .Where(vote => vote.BattleBook.Battle.IsActive)
+            .SingleAsync(vote => vote.UserId == currentUserId, cancellationToken);
+
+        currentUserVote.VotingPointCount += request.VotingPointCount;
+        currentUser.VotingPointCount -= request.VotingPointCount;
+
+        await Context.SaveChangesAsync(cancellationToken);
+    }
+
+    private async Task Validate(SpendVotingPointsCommand request, CancellationToken cancellationToken)
+    {
+        int currentUserId = LoggedUserAccessor.GetCurrentUserId();
+
+        var existCurrentUserVote = await Context.Battles
+            .Where(battle => battle.IsActive)
+            .SelectMany(battle => battle.BattleBooks)
+            .SelectMany(battleBook => battleBook.Votes)
+            .AnyAsync(vote => vote.UserId == currentUserId, cancellationToken);
+
+        if (!existCurrentUserVote)
+        {
+            throw new NotFoundException("Current user has no vote in current battle.");
+        }
+
+        int userVotingPointCount = await Context.Users
+            .Where(user => user.Id == currentUserId)
+            .Select(user => user.VotingPointCount)
+            .SingleAsync(cancellationToken);
+
+        if (request.VotingPointCount > userVotingPointCount)
+        {
+            throw new BadRequestException("Not enough voting points.");
+        }
+    }
+}
+
