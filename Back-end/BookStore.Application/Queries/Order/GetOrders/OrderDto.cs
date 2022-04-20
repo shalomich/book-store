fec878b3b78859e8e4774fc479ca47b8ffd4e0b0
shalomich <shalomich4769@gmail.com>
2022-04-20 23:02:21 +0700

    Create endpoint for getting orders.

---
+++ Back-end/BookStore.Application/Queries/Order/GetOrders/OrderDto.cs
@@ -0,0 +1,17 @@
+﻿using BookStore.Domain.Enums;
+using System.Collections.Generic;
+
+namespace BookStore.Application.Queries.Order.GetOrders;
+
+public record OrderDto
+{
+    public int Id { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Address { get; set; }
+    public OrderReceiptMethod OrderReceiptMethod { get; set; }
+    public PaymentMethod PaymentMethod { get; set; }
+    public IEnumerable<OrderProductDto> Products { get; set; }
+}
+
