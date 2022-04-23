edc461daff35f0d07d82b5f14f8c6d539a671ba7
Roman Simernin <shalomich4769@gmail.com>
2022-04-23 18:11:23 +0700

    Add state to order dto.

--- Back-end/BookStore.Application/Queries/Order/GetOrders/OrderDto.cs
+++ Back-end/BookStore.Application/Queries/Order/GetOrders/OrderDto.cs
@@ -6,12 +6,13 @@ namespace BookStore.Application.Queries.Order.GetOrders;
 public record OrderDto
 {
     public int Id { get; set; }
+    public string State { get; set; }
     public string UserName { get; set; }
     public string Email { get; set; }
     public string PhoneNumber { get; set; }
     public string Address { get; set; }
-    public OrderReceiptMethod OrderReceiptMethod { get; set; }
-    public PaymentMethod PaymentMethod { get; set; }
+    public string OrderReceiptMethod { get; set; }
+    public string PaymentMethod { get; set; }
     public IEnumerable<OrderProductDto> Products { get; set; }
 }
 
