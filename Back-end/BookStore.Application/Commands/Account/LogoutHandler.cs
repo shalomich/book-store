1851801f729085bd27f77a094160121122345ed2
Roman Simernin <shalomich4769@gmail.com>
2022-04-27 23:15:26 +0700

    Fix bug with refresh token.

--- Back-end/BookStore.Application/Commands/Account/LogoutHandler.cs
+++ Back-end/BookStore.Application/Commands/Account/LogoutHandler.cs
@@ -1,36 +1,39 @@
-﻿using BookStore.Application.Dto;
-using BookStore.Application.Exceptions;
-using BookStore.Application.Providers;
+﻿using BookStore.Application.Exceptions;
 using BookStore.Application.Services;
 using BookStore.Domain.Entities;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace BookStore.Application.Commands.Account;
 
-public record LogoutCommand(User User, string RefreshToken) : IRequest<Unit>;
+public record LogoutCommand(string RefreshToken) : IRequest<Unit>;
 internal class LogoutHandler : IRequestHandler<LogoutCommand, Unit>
 {
 	private const string WrongRefreshTokenMessage = "Wrong refresh token";
 
 	private RefreshTokenRepository RefreshTokenRepository { get; }
-    public LogoutHandler(RefreshTokenRepository refreshTokenRepository, UserManager<User> userManager)
+	private LoggedUserAccessor LoggedUserAccessor { get; }
+	private UserManager<User> UserManager { get; }
+	public LogoutHandler(RefreshTokenRepository refreshTokenRepository, UserManager<User> userManager, 
+		LoggedUserAccessor loggedUserAccessor)
     {
-        RefreshTokenRepository = refreshTokenRepository ?? throw new ArgumentNullException(nameof(refreshTokenRepository));
+		RefreshTokenRepository = refreshTokenRepository;
+		LoggedUserAccessor = loggedUserAccessor;
+		UserManager = userManager;
     }
 
     public async Task<Unit> Handle(LogoutCommand request, CancellationToken cancellationToken)
 	{
-		var (user, refreshToken) = request;
+		int userId = LoggedUserAccessor.GetCurrentUserId();
 
-		if (await RefreshTokenRepository.IsValid(refreshToken, user) == false)
+		var user = await UserManager.FindByIdAsync(userId.ToString());
+
+		if (user == null)
+			throw new NotFoundException("Wrong access token.");
+
+		if (await RefreshTokenRepository.IsValid(request.RefreshToken, user) == false)
 			throw new BadRequestException(WrongRefreshTokenMessage);
 
 		await RefreshTokenRepository.Remove(user);
