b95b92f674dea814ba5e3a6c31f6ee04eee7fd20
Roman Simernin <shalomich4769@gmail.com>
2022-05-07 21:41:37 +0700

    Add battle extension.

---
+++ Back-end/BookStore.Application/Commands/Battles/TryExtendBattle/TryExtendBattleResult.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BookStore.Application.Commands.Battles.TryExtendBattle;
+public record TryExtendBattleResult()
+{
+    public bool NeedToExtend { get; init; }
+    public DateTimeOffset? NewEndDate { get; init; }
+}
