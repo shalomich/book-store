8124fa684a8e3b8f22cc7771d94689b83d838bb1
Roman Simernin <shalomich4769@gmail.com>
2022-05-21 13:59:43 +0700

    Create entity TagGroup #263

--- Back-end/BookStore.Domain/Entities/Products/Tag.cs
+++ Back-end/BookStore.Domain/Entities/Products/Tag.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+
 
 namespace BookStore.Domain.Entities.Products
 {
     public class Tag : RelatedEntity
     {
+        public TagGroup TagGroup { set; get; }
+        public int? TagGroupId { set; get; }
         public ISet<ProductTag> ProductTags { set; get; }
         public ISet<User> Users { set; get; }
     }
---
+++ Back-end/BookStore.Domain/Entities/Products/TagGroup.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BookStore.Domain.Entities.Products;
+public class TagGroup : RelatedEntity
+{
+    public IEnumerable<Tag> Tags { get; set; } = Enumerable.Empty<Tag>();
+}
+
