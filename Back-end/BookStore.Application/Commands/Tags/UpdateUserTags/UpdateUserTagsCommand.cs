ea8c830432c0c9245964c6e0bc7f7f86029bbf6d
Roman Simernin <shalomich4769@gmail.com>
2022-05-22 00:16:52 +0700

    Replace add and remove user tag command with update #263

---
+++ Back-end/BookStore.Application/Commands/Tags/UpdateUserTags/UpdateUserTagsCommand.cs
@@ -0,0 +1,55 @@
+﻿using BookStore.Application.Exceptions;
+using BookStore.Application.Services;
+using BookStore.Domain.Entities.Products;
+using BookStore.Persistance;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.Commands.Tags.UpdateUserTags
+{
+    public record UpdateUserTagsCommand(UserTagsDto TagDto) : IRequest;
+    internal class UpdateUserTagsCommandHandler : AsyncRequestHandler<UpdateUserTagsCommand>
+    {
+        public ApplicationContext Context { get; }
+        public LoggedUserAccessor LoggedUserAccessor { get; }
+
+        public UpdateUserTagsCommandHandler(
+            ApplicationContext context,
+            LoggedUserAccessor loggedUserAccessor)
+        {
+            Context = context;
+            LoggedUserAccessor = loggedUserAccessor;
+        }
+        protected override async Task Handle(UpdateUserTagsCommand request, CancellationToken cancellationToken)
+        {
+            var tagIds = request.TagDto.TagIds;
+
+            var newTags = await Context.Tags
+                .Where(tag => tagIds.Contains(tag.Id))
+                .ToListAsync(cancellationToken);
+
+            var notExistTagIds = tagIds.Except(newTags
+                .Select(tag => tag.Id));
+
+            if (notExistTagIds.Any())
+            {
+                var notExistTagIdsString = notExistTagIds
+                    .Select(id => id.ToString())
+                    .Aggregate((id1, id2) => $"{id1}, {id2}");
+
+                throw new NotFoundException("There are not tags with ids: " + notExistTagIdsString);
+            }
+
+            var currentUser = await Context.Users
+                .Include(user => user.Tags)
+                .SingleAsync(user => user.Id == LoggedUserAccessor.GetCurrentUserId(), cancellationToken);
+
+            currentUser.Tags = newTags.ToHashSet();
+
+            await Context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ Back-end/BookStore.Application/Commands/Tags/UpdateUserTags/UserTagsDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace BookStore.Application.Commands.Tags.UpdateUserTags;
+public record UserTagsDto
+{
+    [Required]
+    public IEnumerable<int> TagIds { get; init; } = Enumerable.Empty<int>();
+}
+
