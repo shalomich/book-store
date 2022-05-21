2f78623ea173bee52e4d16490ca17563badaf9ca
Roman Simernin <shalomich4769@gmail.com>
2022-05-21 13:59:43 +0700

    Create entity TagGroup #263

---
+++ Back-end/BookStore.Persistance/DatabaseConfigs/TagGroupDbConfig.cs
@@ -0,0 +1,22 @@
+﻿
+using BookStore.Domain.Entities;
+using BookStore.Domain.Entities.Books;
+using BookStore.Domain.Entities.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Persistance.DatabaseConfigs
+{
+    public class TagGroupDbConfig : RelatedEntityDbConfig<TagGroup>
+    {
+        public override void Configure(EntityTypeBuilder<TagGroup> builder)
+        {
+            base.Configure(builder);
+            builder.HasIndex(tagGroup => tagGroup.Name).IsUnique();
+        }
+    }
+}
