a29209e3af163dd613761240d259275363b1bd09
Roman Simernin <shalo6947@gmail.com>
2022-05-24 16:13:37 +0700

    Create DatabaseInitializer #277

---
+++ Back-end/BookStore.Persistance/DatabaseInitializer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using BookStore.Domain.Enums;
+using Extensions.Hosting.AsyncInitialization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace BookStore.Persistance;
+
+internal sealed class DatabaseInitializer : IAsyncInitializer
+{
+    private ApplicationContext Context { get; }
+
+    public DatabaseInitializer(
+        ApplicationContext context)
+    {
+        Context = context;
+    }
+
+    public async Task InitializeAsync()
+    {
+        await Context.Database.MigrateAsync();
+        await InitializeRolesAsync();
+    }
+
+    private async Task InitializeRolesAsync()
+    {
+        foreach (RoleName roleName in Enum.GetValues<RoleName>())
+        {
+            var roleNameString = roleName.ToString();
+
+            if (await Context.Roles.AnyAsync(role => role.Name == roleNameString))
+            {
+                continue;
+            }
+
+            var userRole = new IdentityRole<int>
+            {
+                Name = roleNameString,
+                NormalizedName = roleNameString.ToUpper(),
+            };
+
+            await Context.Roles.AddAsync(userRole);
+            await Context.SaveChangesAsync();
+        }
+    }
+}
--- Back-end/BookStore.WebApi/Program.cs
+++ Back-end/BookStore.WebApi/Program.cs
@@ -11,9 +11,13 @@ namespace BookStore.WebApi
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            await host.InitAsync();
+
+            await host.RunAsync();    
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
