e222dcd42aac038f5f7a1513fd2760f23c91387f
Roman Simernin <shalomich4769@gmail.com>
2022-06-13 16:06:28 +0700

    Add initialization default book related entities #277

--- Back-end/BookStore.Persistance/DatabaseInitializer.cs
+++ Back-end/BookStore.Persistance/DatabaseInitializer.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using BookStore.Domain.Entities;
 using BookStore.Domain.Enums;
+using BookStore.Persistance.Data;
 using Extensions.Hosting.AsyncInitialization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -25,6 +30,7 @@ internal sealed class DatabaseInitializer : IAsyncInitializer
     {
         await Context.Database.MigrateAsync();
         await InitializeRolesAsync();
+        await InitializeBookRelatedEntities();
     }
 
     private async Task InitializeRolesAsync()
@@ -47,5 +53,57 @@ internal sealed class DatabaseInitializer : IAsyncInitializer
             await Context.Roles.AddAsync(userRole);
             await Context.SaveChangesAsync();
         }
+    }
+
+    private async Task InitializeBookRelatedEntities()
+    {
+        var defaultData = await GetDataFromFile();
+
+        await TryAddBookRelatedEntities(Context.Genres, defaultData.Genres);
+        await TryAddBookRelatedEntities(Context.BookTypes, defaultData.BookTypes);
+        await TryAddBookRelatedEntities(Context.AgeLimits, defaultData.AgeLimits);
+        await TryAddBookRelatedEntities(Context.CoverArts, defaultData.CoverArts);
+        await TryAddBookRelatedEntities(Context.TagGroups, defaultData.TagGroups);
+    }
+
+    private async Task<DefaultBookRelatedEntities> GetDataFromFile()
+    {
+        var resourceFileName = "BookRelatedEntities.json";
+
+        var resourceNamespace = typeof(DefaultBookRelatedEntities).Namespace;
+        var resource = $"{resourceNamespace}.{resourceFileName}";
+
+        var assembly = Assembly.GetExecutingAssembly();
+        using var resourceFile = assembly.GetManifestResourceStream(resource);
+
+        if (resource is null)
+        {
+            throw new InvalidOperationException($"Cannot to load the file with teeth {resource}.");
+        }
+
+        using var reader = new StreamReader(resourceFile);
+
+        var resourceJson = await reader.ReadToEndAsync();
+
+        return JsonSerializer.Deserialize<DefaultBookRelatedEntities>(resourceJson);
+    }
+
+    private async Task TryAddBookRelatedEntities(IQueryable<RelatedEntity> relatedEntityQuery, IEnumerable<RelatedEntity> relatedEntities)
+    {
+        var relatedEntityNames = await relatedEntityQuery
+            .Select(relatedEntity => relatedEntity.Name)
+            .ToListAsync();
+
+        var relatedEntitiesToAdd = relatedEntities
+            .Where(relatedEntity => !relatedEntityNames.Contains(relatedEntity.Name))
+            .ToList();
+
+        if (!relatedEntitiesToAdd.Any())
+        {
+            return;
+        }
+
+        Context.AddRange(relatedEntitiesToAdd);
+        await Context.SaveChangesAsync();
     }
 }
