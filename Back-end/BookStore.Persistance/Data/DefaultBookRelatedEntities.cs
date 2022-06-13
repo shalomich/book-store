8b08f6a7287d3fe4ea0e174bb5eeb6f69fad62ad
Roman Simernin <shalomich4769@gmail.com>
2022-06-13 16:06:28 +0700

    Add initialization default book related entities #277

---
+++ Back-end/BookStore.Persistance/Data/DefaultBookRelatedEntities.cs
@@ -0,0 +1,14 @@
+﻿using BookStore.Domain.Entities.Books;
+using BookStore.Domain.Entities.Products;
+using System.Collections.Generic;
+
+namespace BookStore.Persistance.Data;
+internal class DefaultBookRelatedEntities
+{
+    public IEnumerable<Genre> Genres { get; set; } = new List<Genre>();
+    public IEnumerable<BookType> BookTypes { get; set; } = new List<BookType>();
+    public IEnumerable<AgeLimit> AgeLimits { get; set; } = new List<AgeLimit>();
+    public IEnumerable<CoverArt> CoverArts { get; set; } = new List<CoverArt>();
+    public IEnumerable<TagGroup> TagGroups { get; set; } = new List<TagGroup>();
+}
+
