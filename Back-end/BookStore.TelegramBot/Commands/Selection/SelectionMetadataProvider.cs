2c930fc2742c2ed543bb79671a2316fc147b3498
Roman Simernin <shalomich4769@gmail.com>
2022-06-17 15:13:57 +0700

    feat: add commands for selections #300

---
+++ Back-end/BookStore.TelegramBot/Commands/Selection/SelectionMetadataProvider.cs
@@ -0,0 +1,95 @@
+﻿using BookStore.Application.Dto;
+using BookStore.TelegramBot.Extensions;
+using QueryWorker.Args;
+using System.Text;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace BookStore.TelegramBot.Commands.Selection;
+internal class SelectionMetadataProvider
+{
+    private readonly string _previewHtmlTemplate;
+    private Update Update { get; }
+
+    public SelectionMetadataProvider(Update update)
+    {
+        Update = update;
+    }
+
+    public OptionParameters BuildPagging()
+    {
+        int pageNumber;
+
+        var tryGetCommandResult = Update.TryGetCommand();
+
+        if (tryGetCommandResult.HasCommandArgs)
+        {
+            pageNumber = int.Parse(tryGetCommandResult.CommandArgs[0]);
+        }
+        else
+        {
+            pageNumber = 1;
+        }
+
+        return new OptionParameters
+        {
+            Pagging = new PaggingArgs
+            {
+                PageNumber = pageNumber,
+                PageSize = 2
+            }
+        };
+    }
+
+    public long GetChatId()
+    {
+        return Update.GetChatId();
+    }
+
+    public string GetPreviewHtml(TelegramPreviewDto preview)
+    {
+        var builder = new StringBuilder();
+
+        builder.Append($"<b>Название: </b>" +
+            $"<a href=\"{preview.StoreUrl}\"><i>{preview.Name}</i></a>\n");
+        builder.Append($"<b>Автор: </b><i>{preview.AuthorName}</i>\n");
+        builder.Append($"<b>Издатель: </b><i>{preview.PublisherName}</i>\n");
+        builder.Append($"<b>Цена: </b> {preview.Cost}\n");
+
+        if (preview.DiscountCost.HasValue)
+        {
+            builder.Append($"<u><b>Цена со скидкой: </b>{preview.DiscountCost}</u>\n");
+        }
+
+        return builder.ToString();
+    }
+
+    public InlineKeyboardMarkup BuildNavigationButtons(int totalCount)
+    {
+        var pagging = BuildPagging().Pagging;
+        var pageNumber = pagging.PageNumber;
+        var maxPageNumber = (int)Math.Ceiling(totalCount / (double)pagging.PageSize);
+
+        var command = Update.TryGetCommand().Command;
+
+        int previousPage = pageNumber - 1;
+        int nextPage = pageNumber + 1;
+
+        var navigationButtons = new List<InlineKeyboardButton>
+        {
+            InlineKeyboardButton.WithCallbackData(text: "Предыдущая страница", callbackData: $"/{command} {previousPage}"),
+            InlineKeyboardButton.WithCallbackData(text: "Следующая страница", callbackData: $"/{command} {nextPage}")
+        };
+
+        if (pageNumber == 1)
+        {
+            navigationButtons.Remove(navigationButtons.First());
+        }
+        else if (pageNumber == maxPageNumber)
+        {
+            navigationButtons.Remove(navigationButtons.Last());
+        }
+
+        return new InlineKeyboardMarkup(navigationButtons);
+    }
+}
---
+++ Back-end/BookStore.TelegramBot/Extensions/UpdateExtensions.cs
@@ -0,0 +1,79 @@
+﻿using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace BookStore.TelegramBot.Extensions;
+internal static class UpdateExtensions
+{
+    public static long GetChatId(this Update update)
+    {
+        if (update.Type == UpdateType.Message)
+        {
+            return update.Message.Chat.Id;
+        }
+
+        if (update.Type == UpdateType.CallbackQuery)
+        {
+            return update.CallbackQuery.Message.Chat.Id;
+        }
+
+        throw new InvalidOperationException();
+    }
+
+    public static bool IsCommand(this Update update)
+    {
+        var commandLine = GetCommandLine(update);
+
+        if (commandLine == null)
+        {
+            return false;
+        }
+
+        return commandLine.StartsWith('/');
+    }
+
+    public static TryGetCommandResult TryGetCommand(this Update update)
+    {
+        if (!IsCommand(update))
+        { 
+            return TryGetCommandResult.NotCommand();
+        }
+
+        var commandLine = GetCommandLine(update);
+
+        var commandEndIndex = commandLine.IndexOf(" ");
+
+        bool hasCommandArgs = commandEndIndex != -1;
+
+        string command;
+
+        if (hasCommandArgs)
+        {
+            command = commandLine.Substring(1, commandEndIndex - 1);
+            var commandArgs = commandLine.Substring(commandEndIndex + 1)
+                .Split(' ');
+
+            return TryGetCommandResult.IsCommand(command, commandArgs);
+        }
+        else
+        {
+            command = commandLine.Substring(1);
+
+            return TryGetCommandResult.IsCommand(command);
+        }
+    }
+
+    private static string GetCommandLine(Update update)
+    {
+        if (update.Type == UpdateType.Message)
+        {
+            return update.Message.Text;
+        }
+
+        if (update.Type == UpdateType.CallbackQuery)
+        {
+            return update.CallbackQuery.Data;
+        }
+
+        return null;
+    }
+}
