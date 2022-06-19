9cd311efff617baf0591cd70bbd44faf52da5f47
Roman Simernin <shalomich4769@gmail.com>
2022-06-19 19:14:33 +0700

    feat: create command to delete basket product by id #306

---
+++ Back-end/BookStore.TelegramBot/UseCases/Basket/DeleteBasketProduct/DeleteBasketProductCommandProvider.cs
@@ -0,0 +1,26 @@
+﻿using BookStore.TelegramBot.Extensions;
+using Telegram.Bot.Types;
+
+namespace BookStore.TelegramBot.UseCases.Basket.DeleteBasketProduct;
+internal class DeleteBasketProductCommandProvider
+{
+    public Update Update { get; }
+    public DeleteBasketProductCommandProvider(
+        Update update)
+    {
+        Update = update;
+    }
+
+    public long GetChatId()
+    {
+        return Update.CallbackQuery.Message.Chat.Id;
+    }
+
+    public int GetBasketProductIdToDelete()
+    {
+        var commandArgs = Update.TryGetCommand().CommandArgs;
+
+        return int.Parse(commandArgs[0]);
+    }
+}
+
